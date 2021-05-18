8cdb2afa830466eacda632486f781472b434f351
Adrian Wang <=>
2021-05-18 17:02:28 +0800

    修改 mock api url & proxy 環境變數

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,22 +2,23 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // eslint-disable-next-line func-names
 module.exports = function (app) {
-// proxy第一个参数为要代理的路由
-// 第二参数中target为代理後的请求網址，changeOrigin是否改变请求頭
+  const baseURL = process.env.REACT_APP_URL;
+  // proxy 第一個參數為要代理的路由
+  // 第二個參數中 target 為代理後的請求網址，changeOrigin 是否改變請求頭
   app.use(createProxyMiddleware('/auth', {
-    target: 'https://appbankee-t.feib.com.tw/webctrl/aplfx',
+    target: baseURL,
     changeOrigin: true,
   }));
   app.use(createProxyMiddleware('/aplfx', {
-    target: 'https://appbankee-t.feib.com.tw/webctrl/aplfx',
+    target: baseURL,
     changeOrigin: true,
   }));
   app.use(createProxyMiddleware('/dev', {
-    target: 'https://appbankee-t.feib.com.tw/webctrl/aplfx',
+    target: baseURL,
     changeOrigin: true,
   }));
   app.use(createProxyMiddleware('/aplModule', {
-    target: 'https://appbankee-t.feib.com.tw/webctrl/aplfx',
+    target: baseURL,
     changeOrigin: true,
   }));
 };
