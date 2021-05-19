e48f449dcd5cdb554ca56b70fc853957fbc38951
Adrian Wang <=>
2021-05-19 13:25:04 +0800

    解決專案報錯問題 (暫時移除 redux-persist 配置)

--- src/App.js
+++ src/App.js
@@ -4,24 +4,21 @@ import Header from 'components/Header';
 import routes from 'routes';
 
 /* Store */
-import { PersistGate } from 'redux-persist/integration/react';
-import store, { persistor } from './stores';
+import store from './stores';
 
 const App = () => (
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Header />
-      <BrowserRouter>
-        <Switch>
-          {
-            routes.map((route) => {
-              const { path, exact, component } = route;
-              return <Route key={path} path={path} exact={exact} component={component} />;
-            })
-          }
-        </Switch>
-      </BrowserRouter>
-    </PersistGate>
+    <Header />
+    <BrowserRouter>
+      <Switch>
+        {
+          routes.map((route) => {
+            const { path, exact, component } = route;
+            return <Route key={path} path={path} exact={exact} component={component} />;
+          })
+        }
+      </Switch>
+    </BrowserRouter>
   </Provider>
 );
 
