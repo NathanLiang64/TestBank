8767ea827886b76d9ff2fcd4995e3acaf189fb8b
archieWu <yij1471@gmail.com>
2021-05-25 17:30:21 +0800

    圖形密碼設定邏輯完成

--- src/pages/PatternLockSetting/patternLockSetting_2.js
+++ src/pages/PatternLockSetting/patternLockSetting_2.js
@@ -3,28 +3,39 @@ import Alert from 'components/Alert';
 import PatternLockSettingWrapper from './patternLockSetting.style';
 
 const PatternLockSetting2 = () => {
-  const isFirstSetting = useSelector(({ patternLockSetting }) => patternLockSetting.isFirstSetting);
-  const isActive = useSelector(({ patternLockSetting }) => patternLockSetting.isActive);
+  const type = useSelector(({ patternLockSetting }) => patternLockSetting.type);
   const isResultSuccess = useSelector(({ patternLockSetting }) => patternLockSetting.isResultSuccess);
+
+  const textArea = () => {
+    switch (type) {
+      case 2:
+        return (
+          <div>
+            <p>您的圖形密碼已變更成功囉！</p>
+          </div>
+        );
+      case 3:
+        return (
+          <div>
+            <p>您的圖形密碼登入功能取消囉！</p>
+
+          </div>
+        );
+      default:
+        return (
+          <div>
+            <p>您的圖形密碼登入已經啟用囉！</p>
+            <p>下次登入 Bankee 時，可以使用圖形密碼進行快速登入囉！</p>
+          </div>
+        );
+    }
+  };
 
   const SuccessAlertWithContent = () => (
     <>
       <Alert state="success">設定成功</Alert>
       {
-        isFirstSetting && isActive
-          ? (
-            <div>
-              <p>您的圖形密碼登入已經啟用囉！</p>
-              <p>下次登入 Bankee 時，可以使用圖形密碼進行快速登入囉！</p>
-            </div>
-          )
-          : (
-            <p>
-              您的圖形密碼登入
-              { isActive ? '已變更成功' : '功能取消'}
-              囉！
-            </p>
-          )
+        textArea()
       }
     </>
   );
