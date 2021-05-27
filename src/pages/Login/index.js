62196115705d69fa0c3f4aa05aa7be891ec55839
archieWu <yij1471@gmail.com>
2021-05-27 16:57:24 +0800

    新增登入頁驗證 新增request加密 新增response解密 串接登入驗證

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable radix,no-restricted-globals */
 import { Redirect } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { useCheckLocation, usePageInfo } from 'hooks';
+// import { useCheckLocation, usePageInfo } from 'hooks';
+import * as yup from 'yup';
 import {
   Visibility,
   VisibilityOff,
@@ -18,17 +20,67 @@ import {
   FEIBCheckboxLabel,
 } from 'components/elements';
 import theme from 'themes/theme';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Controller, useForm } from 'react-hook-form';
+import { useEffect } from 'react';
 import LoginWrapper from './login.style';
 import { setLoginFormValues } from './stores/actions';
+import e2ee from '../../utilities/E2ee';
+import { userLogin } from '../../apis/loginApi';
+import getJwtKey from '../../utilities/DoGetToken';
+
+const checkID = (id) => {
+  const tab = 'ABCDEFGHJKLMNPQRSTUVXYWZIO';
+  const A1 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3];
+  const A2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5];
+  const Mx = [9, 8, 7, 6, 5, 4, 3, 2, 1, 1];
+
+  if (id.length !== 10) return false;
+  let i = tab.indexOf(id.charAt(0));
+  if (i === -1) return false;
+  let sum = A1[i] + A2[i] * 9;
+
+  for (i = 1; i < 10; i += 1) {
+    const v = parseInt(id.charAt(i));
+    if (isNaN(v)) return false;
+    sum += v * Mx[i];
+  }
+  if (sum % 10 !== 0) return false;
+  return true;
+};
 
 const Login = () => {
+  /**
+   *- 資料驗證
+   */
+  const schema = yup.object().shape({
+    // 身分證
+    identity: yup.string().required('身分證字號尚未輸入，請確認，謝謝。')
+      .test(
+        'check-custID',
+        '輸入錯誤，請重新填寫，謝謝。',
+        (value) => checkID(value),
+      ),
+    account: yup.string().required('使用者代號尚未輸入，請確認，謝謝。')
+      .min(6, '您輸入的使用者代號長度有誤，請重新輸入，謝謝。').max(20, '您輸入的使用者代號長度有誤，請重新輸入，謝謝。'),
+    password: yup.string().required('密碼尚未輸入，請確認，謝謝。')
+      .min(8, '您輸入的密碼長度有誤，請重新輸入，謝謝。').max(20, '您輸入的密碼長度有誤，請重新輸入，謝謝。'),
+  });
+  const {
+    control, handleSubmit, setValue, formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  useEffect(() => {
+    getJwtKey();
+  }, []);
   const userInfo = useSelector(({ login }) => login.userInfo);
   const loginFormValues = useSelector(({ login }) => login.loginFormValues);
 
   const dispatch = useDispatch();
-  const handleChangeInput = (event) => {
-    dispatch(setLoginFormValues({ ...loginFormValues, [event.target.name]: event.target.value }));
-  };
+  // const handleChangeInput = (event) => {
+  //   dispatch(setLoginFormValues({ ...loginFormValues, [event.target.name]: event.target.value }));
+  // };
   const handleClickShowPassword = () => {
     dispatch(
       setLoginFormValues({ ...loginFormValues, showPassword: !loginFormValues.showPassword }),
@@ -37,16 +89,27 @@ const Login = () => {
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
+  const upperId = (e) => {
+    setValue('identity', e.target.value.toUpperCase());
+  };
 
-  useCheckLocation();
-  usePageInfo('/api/login');
+  const onSubmit = async (data) => {
+    data.identity = data.identity.toUpperCase();
+    data.account = await e2ee(data.account);
+    data.password = await e2ee(data.password);
+    const response = await userLogin(data);
+    localStorage.setItem('jwtToken', response);
+  };
+
+  // useCheckLocation();
+  // usePageInfo('/api/login');
 
   return (
     userInfo
       ? <Redirect to="/shortcut" />
       : (
         <LoginWrapper fullScreen>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="title">
               <h3>Hello !</h3>
               <p>To be Friendlier & Smarter</p>
@@ -56,63 +119,86 @@ const Login = () => {
               <div style={{ width: '100%' }}>
                 <FEIBInputAnimationWrapper>
                   <FEIBInputLabel
-                    htmlFor="id"
+                    htmlFor="identity"
                     $color={theme.colors.basic.white}
                   >
-                    身分證字號 / 手機號碼
+                    身分證字號
                   </FEIBInputLabel>
-                  <FEIBInput
-                    id="id"
-                    name="id"
-                    value={loginFormValues.id}
-                    $color={theme.colors.basic.white}
-                    $borderColor={theme.colors.basic.white}
-                    onChange={handleChangeInput}
+                  <Controller
+                    name="identity"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                      <FEIBInput
+                        {...field}
+                        id="identity"
+                        name="identity"
+                        $color={theme.colors.basic.white}
+                        $borderColor={theme.colors.basic.white}
+                        onBlur={(e) => upperId(e)}
+                      />
+                    )}
                   />
                 </FEIBInputAnimationWrapper>
+                <p>{errors.identity?.message}</p>
               </div>
 
               <div style={{ width: '100%' }}>
                 <FEIBInputAnimationWrapper>
                   <FEIBInputLabel
-                    htmlFor="userId"
+                    htmlFor="account"
                     $color={theme.colors.basic.white}
                   >
                     使用者代號
                   </FEIBInputLabel>
-                  <FEIBInput
-                    id="userId"
-                    name="userId"
-                    value={loginFormValues.userId}
-                    $color={theme.colors.basic.white}
-                    $borderColor={theme.colors.basic.white}
-                    onChange={handleChangeInput}
+                  <Controller
+                    name="account"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                      <FEIBInput
+                        {...field}
+                        id="account"
+                        name="account"
+                        $color={theme.colors.basic.white}
+                        $borderColor={theme.colors.basic.white}
+                      />
+                    )}
                   />
                 </FEIBInputAnimationWrapper>
+                <p>{errors.account?.message}</p>
               </div>
 
               <div style={{ width: '100%' }}>
                 <FEIBInputAnimationWrapper>
                   <FEIBInputLabel htmlFor="password" $color={theme.colors.basic.white}>密碼</FEIBInputLabel>
-                  <FEIBInput
-                    id="password"
+                  <Controller
                     name="password"
-                    value={loginFormValues.password}
-                    type={loginFormValues.showPassword ? 'text' : 'password'}
-                    onChange={handleChangeInput}
-                    endAdornment={(
-                      <FEIBIconButton
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                        $iconColor={theme.colors.basic.white}
-                      >
-                        {loginFormValues.showPassword ? <Visibility /> : <VisibilityOff />}
-                      </FEIBIconButton>
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                      <FEIBInput
+                        {...field}
+                        id="password"
+                        name="password"
+                        type={loginFormValues.showPassword ? 'text' : 'password'}
+                        endAdornment={(
+                          <FEIBIconButton
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            $iconColor={theme.colors.basic.white}
+                          >
+                            {loginFormValues.showPassword ? <Visibility /> : <VisibilityOff />}
+                          </FEIBIconButton>
+                        )}
+                        $color={theme.colors.basic.white}
+                        $borderColor={theme.colors.basic.white}
+                      />
                     )}
-                    $color={theme.colors.basic.white}
-                    $borderColor={theme.colors.basic.white}
                   />
+
                 </FEIBInputAnimationWrapper>
+                <p>{errors.password?.message}</p>
               </div>
 
               <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
