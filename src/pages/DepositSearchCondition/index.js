743421236e7c46b99eefd66378e98f7683eebd3d
Adrian Wang <=>
2021-06-18 22:57:51 +0800

    重構交易明細頁條件篩選關鍵字呈現及資料保存方式, 新增交易明細頁清空條件功能, 新增 CheckboxButtonWrapper 組件 checked 參數

--- src/pages/DepositSearchCondition/index.js
+++ src/pages/DepositSearchCondition/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
@@ -9,12 +10,18 @@ import {
 } from 'components/elements';
 import { dateFormatter } from 'utilities/Generator';
 import DepositSearchConditionWrapper from './depositSearchCondition.style';
-import { setOpenInquiryDrawer, setDateRange, setSelectedKeywords } from '../DepositInquiry/stores/actions';
+import {
+  setOpenInquiryDrawer,
+  setDateRange,
+  setKeywords,
+  setCustomKeyword,
+  setDisplayKeywords,
+} from '../DepositInquiry/stores/actions';
 
-/* eslint-disable */
 // TODO: 若已選的關鍵字未清空，點擊 search icon 應代入原先已選的關鍵字
 const DepositSearchCondition = () => {
   const keywords = useSelector(({ depositInquiry }) => depositInquiry.keywords);
+  const customKeyword = useSelector(({ depositInquiry }) => depositInquiry.customKeyword);
   const dateRange = useSelector(({ depositInquiry }) => depositInquiry.dateRange);
   const [tabId, setTabId] = useState('0');
   const [tempDateRange, setTempDateRange] = useState([]);
@@ -26,46 +33,46 @@ const DepositSearchCondition = () => {
     setTempDateRange(rangeData);
   };
 
-  // 將選擇的關鍵字儲存至 redux
-  const saveSelectedKeywords = (keywordData) => {
-    const selected = [];
-    const selectedKeywords = [];
-
-    // 從 data 中將已選取的關鍵字存放至 selected
-    for (let keyword in keywordData) {
-      if (keywordData[keyword]) {
-        selected.push(keyword);
-      }
-    }
-
-    // 將 selected 內的關鍵字名稱比對所有關鍵字，得出對應的中文名稱
-    selected.forEach((selectedKeyword) => {
-      keywords.find((keyword) => {
-        if (keyword.name === selectedKeyword) {
-          selectedKeywords.push(keyword);
-        }
-      });
-    });
-
-    // 將已選取的關鍵字資料儲存至 redux
-    dispatch(setSelectedKeywords(selectedKeywords));
-  };
-
   // 控制 Tabs 頁籤
   const handleChangeTabList = (event, id) => {
     setTabId(id);
   };
 
+  const handleChangeKeywords = (event) => {
+    const tempKeywords = Array.from(keywords);
+    tempKeywords.find((item) => {
+      if (event.target.name === item.name) {
+        item.selected = event.target.checked;
+      }
+    });
+    dispatch(setKeywords(tempKeywords));
+  };
+
   // 點擊查詢按鈕後傳送資料
   const handleClickSearchButton = async (data) => {
+    const { keywordCustom } = data;
     // 關閉底部抽屜
     dispatch(setOpenInquiryDrawer(false));
 
     // 若有選擇日期，則將日期範圍儲存至 redux
     tempDateRange.length > 0 && dispatch(setDateRange(tempDateRange));
 
-    // 判斷是否有選擇關鍵字，並且將 data (關鍵字) 儲存至 redux
-    saveSelectedKeywords(data);
+    // 拷貝一份當前的 keywords 陣列
+    const tempKeywordsString = JSON.stringify(keywords);
+    const tempKeywords = JSON.parse(tempKeywordsString);
+
+    // 將自訂關鍵字添加至 tempKeywords 陣列
+    tempKeywords.push({
+      title: keywordCustom || '無自訂關鍵字',
+      name: 'keywordCustom',
+      selected: keywordCustom.length > 0,
+    });
+    // 將自訂關鍵字內容存至 redux
+    dispatch(setCustomKeyword(keywordCustom));
+
+    // 若未選取任何關鍵字，則 dispatch 空陣列至 displayKeywords，有選擇則 dispatch 拷貝後的 keywords 陣列
+    const nonSelected = tempKeywords.every((item) => item.selected === false);
+    nonSelected ? dispatch(setDisplayKeywords([])) : dispatch(setDisplayKeywords(tempKeywords));
 
     // TODO: send data
     data.dateRange = tempDateRange;
@@ -135,6 +142,8 @@ const DepositSearchCondition = () => {
               key={keyword.name}
               label={keyword.title}
               register={{ ...register(keyword.name) }}
+              onChange={handleChangeKeywords}
+              checked={keyword.selected}
             />
           ))
         }
@@ -144,6 +153,8 @@ const DepositSearchCondition = () => {
         <FEIBInput
           name="keywordCustom"
           placeholder="請輸入，最多可輸入16個字元"
+          defaultValue={customKeyword}
+          autoFocus
           {...register('keywordCustom', { maxLength: 16 })}
         />
       </div>
