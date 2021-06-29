980276e7cc55a1fc276cc1e74aa17c3ca9629e0d
Adrian Wang <=>
2021-06-29 16:19:28 +0800

    重構: 捲動明細列表時自動 Tabs 切換功能, 重構: 點擊月份 Tab 時明細列表內容自動滾動至該月份的首張卡片

--- src/pages/DepositInquiry/index.js
+++ src/pages/DepositInquiry/index.js
@@ -23,7 +23,7 @@ import {
 
 const DepositInquiry = () => {
   const transactionDetailRef = useRef();
-  const [tabId, setTabId] = useState('detailList12');
+  const [tabId, setTabId] = useState('');
   const [openDownloadDrawer, setOpenDownloadDrawer] = useState(false);
   const [openDetailDialog, setOpenDetailDialog] = useState(false);
   const [isPending, setIsPending] = useState(false);
@@ -85,69 +85,29 @@ const DepositInquiry = () => {
     // dispatch(setDisplayKeywords([]));
   };
 
-  const scrollSpy = () => {
-    transactionDetailRef.current.addEventListener('scroll', () => {
-      const children = Array.from(transactionDetailRef.current.children);
-      const childrenLength = children.length;
-      if ((childrenLength > 1) && children[1].getBoundingClientRect().top > 283) {
-        setTabId(children[0].id);
-      } else if ((childrenLength > 2) && children[2].getBoundingClientRect().top > 283) {
-        setTabId(children[1].id);
-      } else if ((childrenLength > 3) && children[3].getBoundingClientRect().top > 283) {
-        setTabId(children[2].id);
-      } else if ((childrenLength > 4) && children[4].getBoundingClientRect().top > 283) {
-        setTabId(children[3].id);
-      } else if ((childrenLength > 5) && children[5].getBoundingClientRect().top > 283) {
-        setTabId(children[4].id);
-      } else if ((childrenLength > 6) && children[6].getBoundingClientRect().top > 283) {
-        setTabId(children[5].id);
-      } else if ((childrenLength > 7) && children[7].getBoundingClientRect().top > 283) {
-        setTabId(children[6].id);
-      } else if ((childrenLength > 8) && children[8].getBoundingClientRect().top > 283) {
-        setTabId(children[7].id);
-      } else if ((childrenLength > 9) && children[9].getBoundingClientRect().top > 283) {
-        setTabId(children[8].id);
-      } else if ((childrenLength > 10) && children[10].getBoundingClientRect().top > 283) {
-        setTabId(children[9].id);
-      } else if ((childrenLength > 11) && children[11].getBoundingClientRect().top > 283) {
-        setTabId(children[10].id);
-      } else {
-        setTabId(children[children.length - 1].id);
-      }
-
-      // const detailList11 = document.getElementById('detailList11');
-      // const detailList10 = document.getElementById('detailList10');
-      // // const detailList09 = document.getElementById('detailList09');
-      // if (detailList11 && detailList11.getBoundingClientRect().top > 284) {
-      //   setTabId('detailList12');
-      // } else if (detailList10 && detailList10.getBoundingClientRect().top > 284) {
-      //   setTabId('detailList11');
-      // } else {
-      //   setTabId('detailList10');
-      // }
-    });
-  };
-
-  const handleClickMonthTabs = async (month) => {
-    const monthNumber = month.substr(month.length - 2, 2);
-    setTabId(`detailList${monthNumber}`);
+  const handleClickMonthTabs = async (event) => {
+    // 從點擊的 Tab 連結取得 TabId
+    const tabId = event.currentTarget.href.split('#').pop();
     const response = await doGetInitData('/api/depositInquiry');
-    if (response[month]) {
-      const lastKey = Object.keys(response[month])[Object.keys(response[month]).length - 1];
-      dispatch(setDetailList(response[month]));
-      // const target = document.getElementById(`detailList${monthNumber}`);
-      scrollSpy();
-      const target = document.getElementById(lastKey);
+    if (response.november) {
+      dispatch(setDetailList(response.november));
+      const target = Array.from(transactionDetailRef.current['children']).find((child) => child.id === tabId);
       target.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
 
   /* ===================== test ===================== */
-  const callApi = async () => {
+  const getNewDetailData = async (scrollDirection) => {
+    const newDetailList = [];
     const response = await doGetInitData('/api/depositInquiry');
-    if (response.initData) {
-      console.log(response.initData);
+    if (response.nextCall) {
+      if (scrollDirection === 'up') {
+        newDetailList.push(...response.nextCall, ...detailList);
+      } else if (scrollDirection === 'down') {
+        newDetailList.push(...detailList, ...response.nextCall);
+      }
+      dispatch(setDetailList(newDetailList));
       setIsPending(() => {
         console.log('資料回來了，關閉 isPending');
         return false;
@@ -161,21 +121,36 @@ const DepositInquiry = () => {
       const inViewChildren = viewerChild.filter((item) => item.dataset.inview === "y");
       setInViewChild((prev) => {
         if (prev.length > 0 && inViewChildren.length > 0) {
+          setTabId(inViewChildren[0].id);
+
+          /* ============ 判斷使用者往上捲動或往下捲動 ========== */
+          // 往下捲動
           if (prev[0].dataset.index < inViewChildren[0].dataset.index) {
-            // console.log("往下捲動");
             const prevLastIndex = parseInt(prev[prev.length - 1].dataset.index, 10);
             const viewerChildLastIndex = parseInt(viewerChild[viewerChild.length - 1].dataset.index, 10);
-            console.info("prev's last index", prevLastIndex);
-            console.info("total's last index", viewerChildLastIndex);
+            // console.info("prev's last index", prevLastIndex);
+            // console.info("total's last index", viewerChildLastIndex);
             if (viewerChildLastIndex - prevLastIndex < 5 && viewerChildLastIndex - prevLastIndex > 0) {
               if (!isPending) {
-                console.log(`剩餘數量少於 5 張卡片，call api 獲取從索引第 ${viewerChildLastIndex + 1} 到第 ${viewerChildLastIndex + 21} 張`);
+                // console.log(`下方剩餘數量少於 5 張卡片，call api 獲取從索引第 ${viewerChildLastIndex + 1} 到第 ${viewerChildLastIndex + 21} 張`);
                 setIsPending(true);
-                callApi();
+                getNewDetailData('down');
               }
             }
+          //  往上捲動
           } else if (prev[0].dataset.index > inViewChildren[0].dataset.index) {
-            console.log("往上捲動");
+            const prevFirstIndex = parseInt(prev[0].dataset.index, 10);
+            const viewerChildFirstIndex = parseInt(viewerChild[0].dataset.index, 10);
+            // 往上捲動
+            console.info('prevFirstIndex: ', prevFirstIndex);
+            console.info('viewerChildFirstIndex: ', viewerChildFirstIndex);
+            if (prevFirstIndex - viewerChildFirstIndex < 5 && prevFirstIndex - viewerChildFirstIndex > 0) {
+              if (!isPending) {
+                // console.log(`上方剩餘數量少於 5 張卡片，call api 獲取從索引第 ${viewerChildFirstIndex - 1} 到第 ${viewerChildFirstIndex - 21} 張`);
+                setIsPending(true);
+                getNewDetailData('up');
+              }
+            }
           }
         }
         return inViewChildren;
@@ -225,23 +200,24 @@ const DepositInquiry = () => {
     <div className="tabsArea">
       <FEIBTabContext value={tabId}>
         <FEIBTabList onChange={handleChangeTabList} $size="small" className="tabList">
-          <FEIBTab label="12月" value="detailList12" href="#detailList12" onClick={() => handleClickMonthTabs('month12')} />
-          <FEIBTab label="11月" value="detailList11" href="#detailList11" onClick={() => handleClickMonthTabs('month11')} />
-          <FEIBTab label="10月" value="detailList10" href="#detailList10" onClick={() => handleClickMonthTabs('month10')} />
-          <FEIBTab label="09月" value="detailList09" href="#detailList09" onClick={() => handleClickMonthTabs('month09')} />
-          <FEIBTab label="08月" value="detailList08" href="#detailList08" onClick={() => handleClickMonthTabs('month08')} />
-          <FEIBTab label="07月" value="detailList07" href="#detailList07" onClick={() => handleClickMonthTabs('month07')} />
-          <FEIBTab label="06月" value="detailList06" href="#detailList06" onClick={() => handleClickMonthTabs('month06')} />
-          <FEIBTab label="05月" value="detailList05" href="#detailList05" onClick={() => handleClickMonthTabs('month05')} />
-          <FEIBTab label="04月" value="detailList04" href="#detailList04" onClick={() => handleClickMonthTabs('month04')} />
-          <FEIBTab label="03月" value="detailList03" href="#detailList03" onClick={() => handleClickMonthTabs('month03')} />
+          {/* <FEIBTab label="12月" value="detailList12" href="#detailList12" onClick={() => handleClickMonthTabs('month12')} /> */}
+          <FEIBTab label="12月" value="12" href="#12" onClick={handleClickMonthTabs} />
+          <FEIBTab label="11月" value="11" href="#11" onClick={handleClickMonthTabs} />
+          <FEIBTab label="10月" value="10" href="#10" onClick={handleClickMonthTabs} />
+          <FEIBTab label="09月" value="09" href="#09" onClick={handleClickMonthTabs} />
+          <FEIBTab label="08月" value="08" href="#08" onClick={handleClickMonthTabs} />
+          <FEIBTab label="07月" value="07" href="#07" onClick={handleClickMonthTabs} />
+          <FEIBTab label="06月" value="06" href="#06" onClick={handleClickMonthTabs} />
+          <FEIBTab label="05月" value="05" href="#05" onClick={handleClickMonthTabs} />
+          <FEIBTab label="04月" value="04" href="#04" onClick={handleClickMonthTabs} />
+          <FEIBTab label="03月" value="03" href="#03" onClick={handleClickMonthTabs} />
         </FEIBTabList>
       </FEIBTabContext>
     </div>
   );
 
   // eslint-disable-next-line no-unused-vars
-  const testDetailCardList = (list) => (
+  const renderDetailCardList = (list) => (
     list.map((card) => (
       <VisibilitySensor
         key={card.id}
@@ -268,28 +244,28 @@ const DepositInquiry = () => {
     ))
   );
 
-  // eslint-disable-next-line no-unused-vars
-  const renderDetailCardList = (list) => (
-    Object.keys(list).map((month) => (
-      <section key={month} id={month}>
-        { list[month].map((card) => (
-          <DetailCard
-            id={card.date.substr(0, 2)}
-            key={card.id}
-            avatar={card.avatar}
-            title={card.title}
-            type={card.type}
-            date={card.date}
-            sender={card.sender}
-            amount={card.amount}
-            balance={card.balance}
-            noShadow
-            onClick={() => setOpenDetailDialog(true)}
-          />
-        )) }
-      </section>
-    ))
-  );
+  // 舊方法 -> 暫不刪除
+  // const renderDetailCardList = (list) => (
+  //   Object.keys(list).map((month) => (
+  //     <section key={month} id={month}>
+  //       { list[month].map((card) => (
+  //         <DetailCard
+  //           id={card.date.substr(0, 2)}
+  //           key={card.id}
+  //           avatar={card.avatar}
+  //           title={card.title}
+  //           type={card.type}
+  //           date={card.date}
+  //           sender={card.sender}
+  //           amount={card.amount}
+  //           balance={card.balance}
+  //           noShadow
+  //           onClick={() => setOpenDetailDialog(true)}
+  //         />
+  //       )) }
+  //     </section>
+  //   ))
+  // );
 
   const renderSearchDrawer = (element) => (
     <BottomDrawer
@@ -326,11 +302,6 @@ const DepositInquiry = () => {
     />
   );
 
-  /* eslint-disable */
-  // -------------------------- test -------------------------- //
-  const [scrollTop, setScrollTop] = useState(0);
-  const [called, setCalled] = useState(false);
-
   const init = async () => {
     // 清空查詢條件
     dispatch(setDateRange([]));
@@ -341,21 +312,12 @@ const DepositInquiry = () => {
     // 取得所有存款卡的初始資料
     const response = await doGetInitData('/api/depositInquiry');
     if (response.initData) {
+      setTabId(response.initData[0].id.substr(0, 2));
       dispatch(setDetailList(response.initData));
     }
-
-    // TODO: 待解決，此作法將導致連續發送 api 請求，安裝 react-visibility-sensor 測試
-    // transactionDetailRef.addEventListener('scroll', (event) => {
-    //   if (event.target.scrollTop < 2000) {
-    //     console.log('拿前 50 筆資料');
-    //   } else if ((event.target.scrollHeight - event.target.scrollTop) < 2000 ) {
-    //     console.log('拿後 50 筆資料');
-    //   }
-    // });
   };
 
   useEffect(init, [transactionDetailRef]);
-
   useCheckLocation();
   usePageInfo('/api/depositInquiry');
 
@@ -364,10 +326,9 @@ const DepositInquiry = () => {
       { cardInfo && renderCardArea(cardInfo) }
       <div className="inquiryArea measuredHeight">
         { renderSearchBarArea() }
-        { renderTabs() }
+        { tabId && renderTabs() }
         <div className="transactionDetail" ref={transactionDetailRef}>
-          {/*{ detailList && renderDetailCardList(detailList) }*/}
-          { detailList && testDetailCardList(detailList) }
+          { detailList && renderDetailCardList(detailList) }
         </div>
       </div>
       { renderDetailDialog() }
