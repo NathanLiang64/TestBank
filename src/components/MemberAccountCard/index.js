9c0759d80e208c734eccbfed47d67e8853a5334b
Adrian Wang <=>
2021-07-05 20:41:00 +0800

    新增: MemberAccountCard 列表卡片型態, 新增: 常用帳號列表, 新增: 列表卡片滑動編輯

--- src/components/MemberAccountCard/index.js
+++ src/components/MemberAccountCard/index.js
@@ -1,41 +1,152 @@
-import { AccountCircleRounded } from '@material-ui/icons';
+import { useState } from 'react';
+import {
+  AccountCircleRounded, AddRounded, CreateRounded, DeleteRounded,
+} from '@material-ui/icons';
 import Avatar from 'components/Avatar';
+import BottomDrawer from 'components/BottomDrawer';
 import { FEIBIconButton } from 'components/elements';
 import theme from 'themes/theme';
-import MemberAccountCardWrapper from './memberAccountCard.style';
+import MemberAccountCardWrapper, { MemberDrawerContentWrapper } from './memberAccountCard.style';
 
 /*
 * ==================== MemberAccountCard 組件說明 ====================
 * MemberAccountCard 組件包含了 Avatar 組合成一張會員帳號卡片
 * ==================== MemberAccountCard 可傳參數 ====================
-* 1. name -> 會員名稱
-* 2. avatarSrc -> 會員頭像的圖片路徑
-* 3. branchCode -> 銀行代碼
-* 4. branchName -> 銀行名稱
-* 5. account -> 會員帳號
-* 6. onClick -> 點擊事件
+* 1. listType -> 列表卡片型態
+*    列表卡片擁有列表的樣式，可滑動顯示編輯卡片，且沒有右側 icon
+* 2. name -> 會員名稱
+* 3. avatarSrc -> 會員頭像的圖片路徑
+* 4. branchCode -> 銀行代碼
+* 5. branchName -> 銀行名稱
+* 6. account -> 會員帳號
+* 7. onClick -> 點擊事件
 * */
 
 const MemberAccountCard = ({
+  listType,
   name,
   avatarSrc,
   branchCode,
   branchName,
   account,
-  onClick,
-}) => (
-  <MemberAccountCardWrapper>
-    <Avatar small src={avatarSrc} name={name} />
-    <div className="memberInfo">
-      <h3>{name || '會員'}</h3>
-      <p>{`${branchName}(${branchCode}) ${account}`}</p>
-    </div>
-    <div className="changeMemberButton" onClick={onClick}>
+  // onClick,
+}) => {
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [actionText, setActionText] = useState('');
+  const [moreAction, setMoreAction] = useState({
+    isMoreActionOpen: false,
+    startX: 0,
+    endX: 0,
+  });
+
+  const handleClickSwitchMemberDrawer = () => {
+    setOpenDrawer(true);
+  };
+
+  const handleClickAddMemberButton = () => {
+    setActionText('新增');
+  };
+
+  const handleClickCloseDrawer = () => {
+    setOpenDrawer(false);
+    setActionText('');
+  };
+
+  const renderMemberDrawerContent = () => (
+    <MemberDrawerContentWrapper>
+      <div className="addMemberButtonArea" onClick={handleClickAddMemberButton}>
+        <div className="addMemberButtonIcon">
+          <AddRounded />
+        </div>
+        <span className="addMemberButtonText">新增常用帳號</span>
+      </div>
+      <div className="members">
+        <MemberAccountCard
+          listType
+          name="Robert Fox"
+          branchName="遠東商銀"
+          branchCode="805"
+          account="043000990000"
+        />
+        <MemberAccountCard
+          listType
+          name="Jermey123"
+          branchName="遠東商銀"
+          branchCode="805"
+          account="043000990000"
+        />
+      </div>
+    </MemberDrawerContentWrapper>
+  );
+
+  const renderChangeMemberButton = () => (
+    <div className="changeMemberButton" onClick={handleClickSwitchMemberDrawer}>
       <FEIBIconButton $iconColor={theme.colors.primary.light} $fontSize={2.4}>
         <AccountCircleRounded />
       </FEIBIconButton>
     </div>
-  </MemberAccountCardWrapper>
-);
+  );
+
+  const renderMoreActionMenu = () => (
+    <div className={`moreActionMenu ${moreAction.isMoreActionOpen ? 'show' : ''}`}>
+      <button type="button">
+        <CreateRounded />
+        <span>編輯</span>
+      </button>
+      <button type="button">
+        <DeleteRounded />
+        <span>刪除</span>
+      </button>
+    </div>
+  );
+
+  const handleTouchStart = (event) => {
+    const touch = event.targetTouches[0];
+    setMoreAction({ ...moreAction, startX: touch.pageX });
+    // console.info('start', moreAction.startX);
+  };
+
+  const handleTouchMove = (event) => {
+    const touch = event.targetTouches[0];
+    setMoreAction({ ...moreAction, endX: touch.pageX });
+    // console.info('end', moreAction.endX);
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const handleTouchEnd = () => {
+    // console.info('result-startX', moreAction.startX);
+    // console.info('result-endX', moreAction.endX);
+
+    if ((moreAction.startX > moreAction.endX) && (moreAction.startX - moreAction.endX > 20)) {
+      setMoreAction({ ...moreAction, isMoreActionOpen: true });
+    } else {
+      setMoreAction({ ...moreAction, isMoreActionOpen: false });
+    }
+  };
+
+  return (
+    <>
+      <MemberAccountCardWrapper
+        $listType={listType}
+        onTouchStart={listType && handleTouchStart}
+        onTouchMove={listType && handleTouchMove}
+        onTouchEnd={listType && handleTouchEnd}
+      >
+        <Avatar small src={avatarSrc} name={name} />
+        <div className="memberInfo">
+          <h3>{name || '會員'}</h3>
+          <p>{`${branchName}(${branchCode}) ${account}`}</p>
+        </div>
+        { listType ? renderMoreActionMenu() : renderChangeMemberButton() }
+      </MemberAccountCardWrapper>
+      <BottomDrawer
+        title={`${actionText}常用帳號`}
+        isOpen={openDrawer}
+        onClose={handleClickCloseDrawer}
+        content={renderMemberDrawerContent()}
+      />
+    </>
+  );
+};
 
 export default MemberAccountCard;
--- src/components/MemberAccountCard/memberAccountCard.style.js
+++ src/components/MemberAccountCard/memberAccountCard.style.js
@@ -3,9 +3,21 @@ import styled from 'styled-components';
 const MemberAccountCardWrapper = styled.div`
   display: flex;
   align-items: center;
-  padding: 1.6rem 1.2rem;
-  border-radius: .6rem;
-  background: ${({ theme }) => theme.colors.background.lighterBlue};
+  padding: ${({ $listType }) => ($listType ? '1.2rem .8rem' : '1.6rem 1.2rem')};
+  ${({ $listType, theme }) => (
+    $listType
+      ? (`
+        border-bottom: .1rem solid ${theme.colors.border.lighter};
+        &:first-child {
+          border-top: .1rem solid ${theme.colors.border.lighter};
+        };
+        overflow: hidden;
+      `)
+      : (`
+        border-radius: .6rem;
+        background: ${theme.colors.background.lighterBlue};
+      `)
+  )};
   
   .memberInfo {
     margin-left: 1.2rem;
@@ -28,4 +40,75 @@ const MemberAccountCardWrapper = styled.div`
   }
 `;
 
+const MemberDrawerContentWrapper = styled.div`
+  padding: 0 1.6rem 4rem 1.6rem;
+  
+  .addMemberButtonArea {
+    display: flex;
+    align-items: center;
+    padding: 1.2rem .8rem;
+    border-top: .1rem solid ${({ theme }) => theme.colors.border.lighter};
+  }
+  
+  .addMemberButtonIcon {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    width: 4.4rem;
+    height: 4.4rem;
+    background: ${({ theme }) => theme.colors.primary.light};
+    
+    .MuiSvgIcon-root {
+      font-size: 3.2rem;
+      color: ${({ theme }) => theme.colors.basic.white};
+    }
+  }
+  
+  .addMemberButtonText {
+    margin-left: 1.2rem;
+    font-size: 1.6rem;
+    color: ${({ theme }) => theme.colors.primary.light};
+  }
+  
+  .moreActionMenu {
+    display: flex;
+    position: absolute;
+    top: 0;
+    right: -100%;
+    height: 100%;
+    color: ${({ theme }) => theme.colors.basic.white};
+    transition: all .3s ease-out;
+    
+    &.show {
+      right: 0;
+    }
+    
+    button {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      width: 6.8rem;
+      
+      &:first-child {
+        background: ${({ theme }) => theme.colors.primary.light};
+      }
+      &:last-child {
+        background: ${({ theme }) => theme.colors.state.danger};
+      }
+      
+      .MuiSvgIcon-root {
+        margin-bottom: .4rem;
+        font-size: 2.4rem;
+      }
+      
+      span {
+        font-size: 1.2rem;
+      }
+    }
+  }
+`;
+
 export default MemberAccountCardWrapper;
+export { MemberDrawerContentWrapper };
