1c872552fa82e3c77b0c4a174431f12c1255a816
Adrian Wang <=>
2021-09-01 20:15:54 +0800

    新增: 長按移除我的最愛功能, 新增: 移除按鈕特效, 新增: 我的最愛依順序顯示對應背景圖, 新增: 我的最愛 api 撈取 mock data, 修改: 我的最愛項目空白區塊樣式

--- src/pages/Favorite/favorite.style.js
+++ src/pages/Favorite/favorite.style.js
@@ -32,17 +32,53 @@ const FavoriteDrawerWrapper = styled.div`
       flex-direction: column;
       justify-content: center;
       align-items: center;
-      padding: 0;
+      padding: 0 1.2rem;
       height: 14.8rem;
-      border: .2rem dashed #CCC;
       border-radius: .8rem;
       font-size: 1.6rem;
+      user-select: none;
+      
+      img {
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+      }
       
       .icon {
         font-size: 3.2rem;
         color: ${({ theme }) => theme.colors.text.lightGray};
       }
+      
+      .removeButton {
+        position: absolute;
+        top: .8rem;
+        left: .8rem;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        width: 2.4rem;
+        height: 2.4rem;
+        background: ${({ theme }) => theme.colors.primary.light};
+        animation: scaleUp .4s backwards;
+        transform-origin: center;
+        
+        .MuiSvgIcon-root {
+          font-size: 2.4rem;
+          color: ${({ theme }) => theme.colors.basic.white};
+        }
+      }
     }
+  }
+  
+  @keyframes scaleUp {
+    0% { transform: scale(0); opacity: 0; }
+    30% {opacity: 1; }
+    50% {transform: scale(1.4); }
+    65% {transform: scale(1); }
+    80% {transform: scale(1.2); }
+    100% {transform: scale(1); }
   }
 `;
 
