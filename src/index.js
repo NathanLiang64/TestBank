07849801764c1f7574db7b1fda3f78600c6c95a1
gamanina55 <gamanina55@gmail.com>
2021-09-06 22:49:20 +0800

    router加入basename使war檔掛上jboss能正常導頁

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL || ''}>
       <App />
       <GlobalStyles />
     </BrowserRouter>
