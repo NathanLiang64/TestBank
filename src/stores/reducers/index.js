06d66a9d61d6c500dff064e6dd4c6c580f33d730
Adrian Wang <=>
2021-11-30 18:31:27 +0800

    新增: 新版金融卡掛失補發, 新增: 地址驗證規則, 修改: 金融卡掛失補發的修改地址改為 Dialog 顯示, 重構: 金融卡掛失補發邏輯, 修改: 移除金融卡掛失補發 redux state, 修改: FEIBButton 元件新增 className, 更新: 移除轉帳頁不必要的 API, 更新: 因應 WebCtl 要求更改 API URL

--- src/stores/reducers/index.js
+++ src/stores/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import { reducers as headerReducer } from 'components/Header/stores';
 import { reducers as loginReducer } from 'pages/Login/stores';
-import { reducers as lossReissueReducer } from 'pages/LossReissue/stores';
 import { reducers as billPayReducer } from 'pages/BillPay/stores';
 import { reducers as patternLockSettingReducer } from 'pages/PatternLockSetting/stores';
 // import { reducers as depositOverviewReducer } from 'pages/DepositOverview/stores';
@@ -21,7 +20,6 @@ import { reducers as resultDialogReducer } from 'pages/ResultDialog/stores';
 const reducer = combineReducers({
   header: headerReducer,
   login: loginReducer,
-  lossReissue: lossReissueReducer,
   billPay: billPayReducer,
   patternLockSetting: patternLockSettingReducer,
   // depositOverview: depositOverviewReducer,
