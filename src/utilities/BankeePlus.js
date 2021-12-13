34787e6d1f1abd179086a76a207c7ca9d04fde29
IamSiu <mar6861214@gmail.com>
2021-12-13 13:58:01 +0800

    更新 bankeePlus.js

--- src/utilities/BankeePlus.js
+++ src/utilities/BankeePlus.js
@@ -1,41 +1,75 @@
+import { useHistory } from 'react-router';
+
 /* eslint-disable prefer-template */
 const device = {
   ios: () => /iPhone|iPad|iPod/i.test(navigator.userAgent),
   android: () => /Android/i.test(navigator.userAgent),
 };
 
-function goToFunc(funcName, jsonParams = null) {
-  if (device.ios()) {
-    // const msg = JSON.parse('{"name":"' + funcName + '", "data":"' + jsonParams + '"}');
-    // eslint-disable-next-line no-undef
-    // webkit.messageHandlers.bankeeplus.postMessage(msg);
-    const action = {
-      name: funcName,
-      data: jsonParams,
-    };
-    window.webkit.messageHandlers.jstoapp.postMessage(action);
-    return;
-  }
+const funcStack = {
+  push: (code, params, keepData, hideMenu) => {
+    const stack = JSON.parse(localStorage.getItem('funcStack') ?? '[]');
+    stack.push({
+      code, params, keepData, hideMenu,
+    });
+    localStorage.setItem('funcStack', JSON.stringify(stack));
+  },
+  pop: () => {
+    const stack = JSON.parse(localStorage.getItem('funcStack') ?? '[]');
+    const funcItem = stack.pop();
+    localStorage.setItem('funcStack', JSON.stringify(stack));
+    return funcItem;
+  },
+  clear: () => {
+    localStorage.setItem('funcStack', '[]');
+  },
+};
 
-  if (device.android()) {
+function goToFunc(funcName, jsonParams = null) {
+  console.log(navigator.userAgent);
+  // console.debug('name:' + funcName + ', data:' + jsonParams);
+  if (device.ios()) {
+    const msg = JSON.parse('{"name":"' + funcName + '", "data":"' + jsonParams + '"}');
+    // eslint-disable-next-line no-undef
+    window.webkit.messageHandlers.jstoapp.postMessage(msg);
+  } else if (device.android()) {
     window.jstoapp[funcName](jsonParams);
+  } else {
+    console.log(`[Start Function(${funcName})]`);
+    funcStack.push(funcName, jsonParams);
+
+    const history = useHistory();
+    history.push();
   }
 }
 
-function closeFunc(jsonParams = null) {
+// 觸發APP返回上一頁功能
+function closeFunc() {
   if (device.ios()) {
-    const msg = JSON.parse('{"name":"closefunc"}');
+    const msg = JSON.parse('{"name":"closeFunc"}');
     // eslint-disable-next-line no-undef
-    webkit.messageHandlers.bankeeplus.postMessage(msg);
-    return;
-  }
+    // webkit.messageHandlers.bankeeplus.postMessage(msg);
+    window.webkit.messageHandlers.jstoapp.postMessage(msg);
+  } else if (device.android()) {
+    window.jstoapp.closeFunc();
+  } else {
+    const funcItem = funcStack.pop();
+    if (funcItem) {
+      console.log(`[Close Function and Back to(${funcItem.funcName})]`);
 
-  if (device.android()) {
-    window.bankeeplus.closefunc(jsonParams);
+      const history = useHistory();
+      history.push(funcItem.funcName);
+    }
   }
+}
+
+function goHome() {
+  funcStack.clear();
+  goToFunc('/more');
 }
 
 export {
   goToFunc,
   closeFunc,
+  goHome,
 };
