bd05501b2d1d850253eb2c2d1c6147e73af3a8ff
IamSiu <mar6861214@gmail.com>
2021-12-14 10:06:07 +0800

    git script test

--- public/appToWebFunc.js
+++ public/appToWebFunc.js
@@ -1,6 +1,9 @@
 /* eslint-disable */
 function setEnCrydata(event) {
   alert(JSON.stringify(event));
+  // Crydata: aes, Enivec: iv
+  const { Crydata, Enivec } = event;
+  
 };
 
 console.log('load app to webview functions success');
