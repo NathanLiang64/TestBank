40ae64e7653fb0898cd305a7828fb994417fb44a
IamSiu <mar6861214@gmail.com>
2021-12-14 10:16:02 +0800

    新增 js-cookie，取得 jwtToken 測試

--- public/appToWebFunc.js
+++ public/appToWebFunc.js
@@ -3,7 +3,8 @@ function setEnCrydata(event) {
   alert(JSON.stringify(event));
   // Crydata: aes, Enivec: iv
   const { Crydata, Enivec } = event;
-  
+  alert('Crydata: ' + Crydata);
+  alert('Enivec: ' + Enivec);
 };
 
 console.log('load app to webview functions success');
