050d1af70ee9cab8966f01e7af53f6da3ba2aa77
iamsiutw <mar6861214@gmail.com>
2021-12-23 11:08:53 +0800

    調整 axios config show web log 測試

--- public/funcsCalledByApp.js
+++ public/funcsCalledByApp.js
@@ -23,8 +23,8 @@ function setEnCrydata(event) {
   const { Crydata, Enivec } = event;
   const aesKey = window.atob(Crydata).substr(7);
   const iv = window.atob(Enivec).substr(7);
-  localStorage.setItem('iv', iv);
   localStorage.setItem('aesKey', aesKey);
+  localStorage.setItem('iv', iv);
   switchLoadingInTag(false)
 };
 
