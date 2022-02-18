b5a561d665605d6d4744a62d6400c83332d53ee0
iamsiutw <mar6861214@gmail.com>
2022-02-18 11:40:48 +0800

    串接查詢預約轉帳api

--- src/pages/ReserveTransferSearch/detailContent.js
+++ src/pages/ReserveTransferSearch/detailContent.js
@@ -1,31 +1,39 @@
+import { toCurrency } from 'utilities/Generator';
 import InformationList from 'components/InformationList';
 import DialogContentWrapper from './dialogContent.style';
 
-const DetailContent = ({ data }) => (
+const DetailContent = ({ contentData: { data, selectedAccount } }) => (
   <DialogContentWrapper>
+    {
+      console.log(data)
+    }
     <div className="mainBlock">
       <div className="dataLabel">轉出金額與轉入帳號</div>
-      <div className="balance">{data.amount}</div>
+      <div className="balance">
+        $
+        {data.amount}
+      </div>
       <div className="account">
-        遠東商銀(
-        { data.bankCode }
+        { data.inBankName }
+        (
+        { data.inBank }
         )
       </div>
       <div className="account">{ data.inActNo }</div>
     </div>
     <div className="informationListContainer">
-      <InformationList title="轉出帳號" content={data.outActNo} remark="保時捷車友會" />
-      <InformationList title="預約轉帳日" content={data.trnsDate} />
-      <InformationList title="週期" content={data.bookType === '1' ? '單次' : '週期'} remark={data.bookType === '1' ? '' : '預計轉帳10次｜成功2次｜失敗0次'} />
+      <InformationList title="轉出帳號" content={selectedAccount.accountId} remark={selectedAccount.showName} />
+      <InformationList title="預約轉帳日" content={data.payDate} />
+      <InformationList title="週期" content={data.payDateWording} />
       {
-        data.bookType === '2' && (<InformationList title="期間" content="2021/05/01~2022/02/28" />)
+        data.chargeMode !== '1' && (<InformationList title="期間" content={`${data.payDate}~${data.payDateEnd}`} />)
       }
-      <InformationList title="預約設定日" content="2021/03/05" />
-      {
-        data.bookType === '2' && (<InformationList title="預約轉帳總金額" content="$200,000" />)
-      }
-      <InformationList title="帳戶餘額" content="$20,000" />
-      <InformationList title="備註" content={data.remark} />
+      <InformationList title="預約設定日" content={data.trnsDate} />
+      {/* {
+        data.chargeMode !== '1' && (<InformationList title="預約轉帳總金額" content="$200,000" />)
+      } */}
+      <InformationList title="帳戶餘額" content={`$${toCurrency(selectedAccount.balance)}`} />
+      <InformationList title="備註" content={data.memo} />
     </div>
   </DialogContentWrapper>
 );
