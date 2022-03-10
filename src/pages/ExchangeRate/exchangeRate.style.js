43979a2f9848239f08f2f53f501d606710f0d1c8
iamsiutw <mar6861214@gmail.com>
2022-03-10 12:19:40 +0800

    新增 匯率 頁

---
+++ src/pages/ExchangeRate/exchangeRate.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import Layout from 'components/Layout';
+
+const ExchangeRateWrapper = styled(Layout)`
+`;
+
+export default ExchangeRateWrapper;
---
+++ src/pages/ExchangeRate/index.js
@@ -0,0 +1,23 @@
+import { useGetEnCrydata } from 'hooks';
+
+/* Elements */
+import Header from 'components/Header';
+import ExchangeTable from 'pages/Exchange/exchangeTable';
+
+/* Styles */
+import ExchangeRateWrapper from './exchangeRate.style';
+
+const ExchangeRate = () => {
+  useGetEnCrydata();
+
+  return (
+    <>
+      <Header title="匯率" />
+      <ExchangeRateWrapper>
+        <ExchangeTable />
+      </ExchangeRateWrapper>
+    </>
+  );
+};
+
+export default ExchangeRate;
