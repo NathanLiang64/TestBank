32d9df28c6df85acd6c54e2e50aab3ac99d197bf
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2022-05-18 11:44:36 +0800

    Extend InfoArea for use in DepositPlan

--- src/components/InfoArea/index.js
+++ src/components/InfoArea/index.js
@@ -7,15 +7,24 @@ import InfoAreaWrapper from './infoArea.style';
 * 1. className -> 若需在某些頁面更動樣式，可加上 className 參數傳入 class 名稱
 * 2. space -> 此組件預設無 margin，可傳入 "top"、"bottom"、"both" 字串來產生固定高度的 margin
 * 3. children -> 顯示文字，不須設置 children 屬性，直接在標籤內部填寫文字
+* 4. position: ('top' | 'bottom') -> 改變尖嘴的位子。
+* 5. variants: ('top' | 'bottom') -> 改變樣式。
+* 6. color -> 改變文字顏色。
+* 7. bgColor -> 改變背景顏色。
 * */
 
-const InfoArea = ({ className, space, children }) => (
+const InfoArea = ({ className, space, children, position = 'bottom', variant, color, bgColor }) => (
   <InfoAreaWrapper
     className={className ? `infoArea ${className}` : 'infoArea'}
     $space={space}
+    $position={position}
+    $variant={variant}
+    $color={color}
+    $bgColor={bgColor}
   >
+    { position === 'top' && <div className="polygon top" /> }
     { children }
-    <div className="polygon" />
+    { position === 'bottom' && <div className="polygon bottom" /> }
   </InfoAreaWrapper>
 );
 
--- src/components/InfoArea/infoArea.style.js
+++ src/components/InfoArea/infoArea.style.js
@@ -8,41 +8,73 @@ const handleSpaceType = (position) => {
       return '0 1.6rem 1.6rem 1.6rem';
     case 'both':
       return '1.6rem 1.6rem';
+    case 'auto':
+      return '1.6rem auto';
     default:
       return '0 1.6rem';
   }
 };
 
+const variants = (variant) => {
+  switch (variant) {
+    case 'top':
+      return `
+        padding: .4rem;
+        border-radius: .8rem;
+        font-size: 1.4rem;
+      `;
+    default:
+      return `
+        padding: .4rem;
+        border-radius: .8rem;
+        font-size: 1.2rem;
+      `;
+  }
+}
+
 const InfoAreaWrapper = styled.div`
   margin: ${({ $space }) => handleSpaceType($space)};
-  padding: .8rem;
-  border-radius: .4rem;
-  font-size: 1.2rem;
+  ${({ $space }) => $space === 'auto' && 'min-width: 200px; width: fit-content;'}
+  ${({ $variant }) => variants($variant)}
   text-align: center;
-  color: ${({ theme }) => theme.colors.primary.light};
-  background: ${({ theme }) => theme.colors.primary.lightest};
+  color: ${({ $color, theme }) => $color || theme.colors.primary.light};
+  background: ${({ $bgColor, theme }) => $bgColor || theme.colors.primary.lightest};
   
   * {
-    color: ${({ theme }) => theme.colors.primary.light};
+    color: ${({ $color, theme }) => $color || theme.colors.primary.light};
   }
+
   .polygon {
-    background: ${({ theme }) => theme.colors.primary.lightest};
-    width: 1.4rem;
-    height: 1.2rem;
     position: absolute;
-    right: 19%;
-    bottom: -1.2rem;
     overflow: hidden;
-    border-radius: 0 0 1.2rem 0;
-    &:before {
-      content: ' ';
-      background: #fff;
+
+    &.top {
+      width: 0;
+      height: 0;
+      border-left: 0.5rem solid transparent;
+      border-right: 0.5rem solid transparent;
+      border-bottom: 1rem solid ${({ $bgColor, theme }) => $bgColor || theme.colors.primary.lightest};
+      left: 1.5rem;
+      top: -1rem;
+    }
+
+    &.bottom {
+      background: ${({ $bgColor, theme }) => $bgColor || theme.colors.primary.lightest};
       width: 1.4rem;
       height: 1.2rem;
-      position: absolute;
-      left: -0.7rem;
-      top: 0;
-      border-radius: 0 10rem 5rem 0;
+      border-radius: 0 0 1.2rem 0;
+      right: 19%;
+      bottom: -1.2rem;
+      &:before {
+        content: ' ';
+        background: #fff;
+        width: 1.4rem;
+        height: 1.2rem;
+        position: absolute;
+        left: -0.7rem;
+        top: 0;
+        border-radius: 0 10rem 5rem 0;
+      }
     }
   }
 `;
