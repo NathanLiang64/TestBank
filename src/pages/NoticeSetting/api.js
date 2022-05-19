9170a669cb53911b185d73af268176ba06397483
Jerry Yeh <jerry88634@feib.com.tw>
2022-05-19 20:10:22 +0800

    調整：B00300 推播設定 API

--- src/pages/NoticeSetting/api.js
+++ src/pages/NoticeSetting/api.js
@@ -2,12 +2,13 @@ import { callAPI } from 'utilities/axios';
 
 /**
  * 通知設定綁定
- * @param request {
- *   communityNotice: 是否啟用社群通知 : Y:啟用 , N:關閉
- *   boardNotice: 是否啟用公告通知 : Y:啟用 , N:關閉
- *   securityNotice: 是否啟用安全通知 : Y:啟用 , N:關閉
- *   nightMuteNotice: 是否啟用夜間靜音通知 : Y:啟用 , N:關閉
+ * @param {*} request {
+ *  communityNotice: 是否啟用社群通知 : Y:啟用 , N:關閉
+ *  boardNotice: 是否啟用公告通知 : Y:啟用 , N:關閉
+ *  securityNotice: 是否啟用安全通知 : Y:啟用 , N:關閉
+ *  nightMuteNotice: 是否啟用夜間靜音通知 : Y:啟用 , N:關閉
  * }
+ * @returns
  */
 export const bindPushSetting = async (request) => {
   const response = await callAPI('/api/push/v1/bindPushSetting', request);
@@ -16,14 +17,14 @@ export const bindPushSetting = async (request) => {
 
 /**
  * 查詢客戶近兩個月推播訊息
- * @return[{
- *   msgId: 訊息編號
- *   msgType: 訊息類別, P:公告 A:帳務 C:社群 S:安全
- *   msgOutline: 訊息標題, Base64編碼
- *   msgContent: 訊息簡述, Base64編碼
- *   msgUrl: 優惠導連, 無導連時則空
- *   sendTime: 發送時間
- *   status: 狀態, R:已讀 其他:未讀
+ * @returns [{
+ *  msgId: 訊息編號
+ *  msgType: 訊息類別, P:公告 A:帳務 C:社群 S:安全
+ *  msgOutline: 訊息標題, Base64編碼
+ *  msgContent: 訊息簡述, Base64編碼
+ *  msgUrl: 優惠導連, 無導連時則空
+ *  sendTime: 發送時間
+ *  status: 狀態, R:已讀 其他:未讀
  * }, ...]
  */
 export const queryPushSetting = async () => {
@@ -33,28 +34,24 @@ export const queryPushSetting = async () => {
 
 /**
  * 查詢客戶近兩個月推播訊息
- * @param token
- * @param request { }
- * @return[{
-    msgId: 訊息編號
-    msgType: 訊息類別, P:公告 A:帳務 C:社群 S:安全
-    msgOutline: 訊息標題, Base64編碼
-    msgContent: 訊息簡述, Base64編碼
-    msgUrl: 優惠導連, 無導連時則空
-    sendTime: 發送時間
-    status: 狀態, R:已讀 其他:未讀
-  }, ...]
-  */
+ * @returns [{
+ *  msgId: 訊息編號
+ *  msgType: 訊息類別, P:公告 A:帳務 C:社群 S:安全
+ *  msgOutline: 訊息標題, Base64編碼
+ *  msgContent: 訊息簡述, Base64編碼
+ *  msgUrl: 優惠導連, 無導連時則空
+ *  sendTime: 發送時間
+ *  status: 狀態, R:已讀 其他:未讀
+ * }, ...]
+ */
 export const queryLastPush = async () => {
   const response = await callAPI('/api/push/v1/queryLastPush');
   return response.data;
 };
 
 /**
-   * 推播訊息單筆已讀
- * @param token
- * @param request { msgId }
- * @return
+ * 推播訊息單筆已讀
+ * @returns
  */
 export const chgPushStatus = async () => {
   const response = await callAPI('/api/push/v1/chgPushStatus');
@@ -62,10 +59,9 @@ export const chgPushStatus = async () => {
 };
 
 /**
-   * 推播訊息單筆刪除
- * @param token
- * @param request { msgId }
- * @return
+* 推播訊息單筆刪除
+ * @param {*} request { msgId }
+ * @returns
  */
 export const deletePush = async () => {
   const response = await callAPI('/api/push/v1/deletePush');
@@ -74,9 +70,8 @@ export const deletePush = async () => {
 
 /**
  * 推播訊息全部已讀
- * @param token
  * @param request { msgType }
- * @return
+ * @returns
  */
 export const chgAllPushStatus = async () => {
   const response = await callAPI('/api/push/v1/chgAllPushStatus');
@@ -85,9 +80,8 @@ export const chgAllPushStatus = async () => {
 
 /**
  * 推播訊息全部刪除
- * @param token
  * @param request { msgType }
- * @return
+ * @returns
  */
 export const deleteAllPush = async () => {
   const response = await callAPI('/api/push/v1/deleteAllPush');
