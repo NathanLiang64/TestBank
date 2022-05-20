18139bf4b8c4e35cfc3c178897778df8f19774cd
Jerry Yeh <jerry88634@feib.com.tw>
2022-05-20 15:46:36 +0800

    調整：C00600 存錢計劃 API 欄位及說明。

--- src/pages/C00600_DepositPlan/api.js
+++ src/pages/C00600_DepositPlan/api.js
@@ -6,13 +6,19 @@ import { callAPI } from 'utilities/axios';
  {
     plans: [{ // 進行中的計劃數量
       planId, // 計劃代碼（UUID 型式），可以用來取得圖片。 直接組合 URL/images/dp/plans.planId.jpg
-      progCode, // 方案代碼。
-      imageId, // Title圖片（預設代碼: 1~6），若是由用戶上傳的Base64影像資料，則在建立成功後再透過 updateDepositPlan 上傳
+      progInfo: { // 存錢計劃適用方案(Program)資訊
+        code: 代碼，應跟優利的代碼一致。
+        type: 類型：0.基本方案, 1.行銷活動方案.
+        name: 名稱
+        rate: 計息利率，例: 0.6；僅供顯示用，不會拿來計息。
+        description: 詳細說明。
+      },
+      imageId, // Title圖片（預設代碼: 0,1~6），若是由用戶上傳的Base64影像資料，則在建立成功後再透過 updateDepositPlan 上傳
       name, // 計劃名稱（最多七個字），若為 progCode=行銷活動方案 則為方案名稱。
       startDate, // 計劃啟動時間，格式：yyyyMMdd
       endDate, // 計劃結束時間，格式：yyyyMMdd
       cycleMode, // 存入週期（1.每周、2.每月）
-      cycleTime, // 存入時機；每周：0～6(周日～周六)、每月1~28及月底(31)
+      cycleTiming, // 存入時機；每周：0～6(周日～周六)、每月：1~28及月底(31)
       amount, // 每期存入金額，格式：99999。
       goalAmount: // 依 amount * 期數 計算出的計劃目標存款金額。
       bindAccountNo, // 綁定的子帳戶（帳號、餘額）
@@ -30,7 +36,7 @@ export const getDepositPlans = async () => {
 };
 
 /**
- * 取得存錢計劃適用方案(Program)資訊。
+ * 取得存錢計劃適用方案(Program)資訊，已停用或不在有效期限內的方案不會列在清單中。
  * @returns {object}
  * @return [{
  *   code: 代碼，應跟優利的代碼一致。
@@ -66,7 +72,7 @@ export const getDepositPlanProgram = async () => {
  *    cycleMode, // 存入週期（1.每周、2.每月）；此欄位不可為空值。
  *    cycleTiming, // 存入時機；每周：0～6(周日～周六)、每月1~28或月底(31)；此欄位不可為空值。
  *    amount, // 每共月存入金額，格式：99999。此欄位不可為空值。
- *    bindAccountNo, // 使用的子帳戶，例：04300491000001；若子帳戶為空值，表示建立新子帳戶。此欄位不可為空值。
+ *    bindAccountNo, // 使用的子帳戶，例：04300491000001；若子帳戶為空值，表示建立新子帳戶。
  * }
  * @returns 計劃代碼（planId, UUID 型式）。有值表示建立成功；若為 null 表示建立失敗，會傳回錯誤。
  */
