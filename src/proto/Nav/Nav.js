102155dff4364192c2f711fdb0539c8f59766632
IamSiu <mar6861214@gmail.com>
2022-05-23 12:05:31 +0800

    nav 新增手機號碼收款設定

--- src/proto/Nav/Nav.js
+++ src/proto/Nav/Nav.js
@@ -47,6 +47,7 @@ const Nav = () => {
           <div onClick={() => startFunc('B00300')}>B00300 訊息通知 - (施工中)</div>
           <div onClick={() => startFunc('S00400')}>S00400 訊息通知設定 - (施工中)</div>
           <div onClick={() => startFunc('E00100')}>E00100 換匯 - (施工中)</div>
+          <div onClick={() => startFunc('T00600')}>T00600 手機號碼收款設定 - (施工中)</div>
         </div>
         {/* <div onClick={() => goToFunc({ route: '/foreignCurrencyPriceSetting', funcID: 'unset' })}>
           <ul>
