49f218becea15afeaadba5bc224578f3eda82550
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2022-05-25 14:47:49 +0800

    Clean up workflow logic

--- src/pages/C00600_DepositPlan/C00600.jsx
+++ src/pages/C00600_DepositPlan/C00600.jsx
@@ -1,47 +1,40 @@
 import { useEffect, useState } from 'react';
 import { useHistory, useLocation } from 'react-router';
 import { useDispatch } from 'react-redux';
+import uuid from 'react-uuid';
 
 import Layout from 'components/Layout/Layout';
 import { MainScrollWrapper } from 'components/Layout';
 import SwiperLayout from 'components/SwiperLayout';
 import { setWaittingVisible } from 'stores/reducers/ModalReducer';
+import { showCustomPrompt } from 'utilities/MessageModal';
 
 import DepositPlanHeroSlide from 'components/DepositPlanHeroSlide';
 import EmptySlide from './components/EmptySlide';
 import EmptyPlan from './components/EmptyPlan';
 import DepositPlan from './components/DepositPlan';
 
-import {
-  showNoMainAccountAlert,
-  showUnavaliableSubAccountAlert,
-  showNonZeroBalanceAlert,
-} from './utils/customPrompts';
-
+import { getAccountSummary } from '../C00300_NtdDeposit/api';
 import { getDepositPlans } from './api';
 
 const renderSlides = (plans) => {
-  const slides = Array.from({ length: 3 }, () => <EmptySlide />);
-
+  const slides = Array.from({ length: 3 }, () => <EmptySlide key={uuid()} />);
   if (plans) {
     plans.forEach((p, i) => {
-      slides[i] = <DepositPlanHeroSlide account={p.bindAccountNo} {...p} />;
+      slides[i] = <DepositPlanHeroSlide key={uuid()} account={p.bindAccountNo} {...p} />;
     });
   }
-
   return slides;
 };
 
 const renderContents = (plans) => {
-  const slides = Array.from({ length: 3 }, () => <EmptyPlan />);
-
+  const slides = Array.from({ length: 3 }, () => <EmptyPlan key={uuid()} />);
   if (plans) {
     plans.forEach((p, i) => {
       const currentValue = p.amount / 10000;
-      slides[i] = <DepositPlan currentValue={currentValue} expireDate={p.endDate} {...p} />;
+      slides[i] = <DepositPlan key={uuid()} currentValue={currentValue} expireDate={p.endDate} {...p} />;
     });
   }
-
   return slides;
 };
 
@@ -57,32 +50,24 @@ const DepositPlanPage = () => {
   useEffect(async () => {
     dispatch(setWaittingVisible(true));
 
-    // TODO: 是否已申請bankee帳戶(台幣)
-    // eslint-disable-next-line
-    if (false) {
-      // TODO：沒有「台幣帳戶」導去申請
-      showNoMainAccountAlert({ onDismiss: () => history.goBack() });
+    // 是否已申請bankee帳戶(台幣)
+    const acctData = await getAccountSummary('MC'); // M=台幣主帳戶、C=台幣子帳戶
+    if (!acctData?.length) {
+      showCustomPrompt({
+        title: '新增存錢計畫',
+        message: '您尚未持有Bankee存款帳戶',
+        // TODO: 沒有「台幣帳戶」導去申請
+        onOk: () => history.goBack(),
+        okContent: '現在就來申請吧!',
+      });
     }
 
-    const res = getDepositPlans();
+    const res = await getDepositPlans();
 
+    // 如果從別的頁面跳轉，並欲想顯示特定計畫...
     if ('focusToAccountNo' in location.state) {
+      // TODO
       console.debug('do something with accountNo', location.state.focusToAccountNo);
-    }
-
-    // 是否已開立8個子帳戶
-    if (res.totalSubAccountCount >= 8) {
-      // TODO: 是否至少一個子帳號是沒有綁定帳本或存錢計畫
-      // eslint-disable-next-line
-      if (false) {
-        showUnavaliableSubAccountAlert({ onDismiss: () => history.goBack() });
-      }
-
-      // TODO: 該子帳戶餘額是否為0
-      // eslint-disable-next-line
-      if (false) {
-        showNonZeroBalanceAlert({ onDismiss: () => history.goBack() });
-      }
     }
 
     setPlans(res.plans);
