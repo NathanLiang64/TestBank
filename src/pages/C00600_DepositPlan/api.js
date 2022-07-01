d5bcbc157eb4b136f13368c5d749a01823140212
brian <xxbrian1031@gmail.com>
2022-07-01 18:00:11 +0800

    增修 存錢計劃 取得交易明細API名稱 加s

--- src/pages/C00600_DepositPlan/api.js
+++ src/pages/C00600_DepositPlan/api.js
@@ -33,7 +33,7 @@ import mockTerms from './terms';
     }
  */
 export const getTransactionDetails = async (request) => {
-  const response = await callAPI('/api/deposit/v1/getTransaction', request);
+  const response = await callAPI('/api/deposit/v1/getTransactions', request);
   return response.data;
 };
 
