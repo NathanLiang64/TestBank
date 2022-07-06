b44c16c4fef1597bacc91fd4fe95e8c1e86bbaf6
IamSiu <mar6861214@gmail.com>
2022-07-06 17:28:57 +0800

    L00200 切版

--- src/routes.js
+++ src/routes.js
@@ -44,6 +44,9 @@ const E001001 = lazy(() => import('pages/E00100_Exchange/exchange_1'));
 const E001002 = lazy(() => import('pages/E00100_Exchange/exchange_2'));
 const E00200 = lazy(() => import('pages/E00200_ExchangeRate'));
 const E00300 = lazy(() => import('pages/E00300_FinancialDepartments'));
+const L00300 = lazy(() => import('pages/L00300_LoanInterest'));
+const L003001 = lazy(() => import('pages/L00300_LoanInterest/loanInterest_1'));
+const L00200 = lazy(() => import('pages/L00200_Principle'));
 const L00100 = lazy(() => import('pages/L00100_Loan/L00100'));
 const L00100Detail = lazy(() => import('pages/L00100_Loan/Details'));
 const L00100Reward = lazy(() => import('pages/L00100_Loan/Rewards'));
@@ -138,6 +141,9 @@ const routes = [
   { path: '/E001002', exact: false, component: E001002 },
   { path: '/E00200', exact: false, component: E00200 },
   { path: '/E00300', exact: false, component: E00300 },
+  { path: '/L00300', exact: false, component: L00300 },
+  { path: '/L003001', exact: false, component: L003001 },
+  { path: '/L00200', exact: false, component: L00200 },
   { path: '/L00100', exact: false, component: L00100 },
   { path: '/L001001', exact: false, component: L00100Reward },
   { path: '/L001002', exact: false, component: L00100Detail },
