d51d5987865a766a724fd526060291f7bfac0e23
Jerry Yeh <jerry88634@feib.com.tw>
2022-07-15 11:36:41 +0800

    修正：常用轉入帳號(D00500) 及 約定轉入帳號(D00600) API

--- src/pages/D00500_FrequentContacts/api.js
+++ src/pages/D00500_FrequentContacts/api.js
@@ -1,64 +1,60 @@
-import { addFavAccount, updateFavAccount, removeFavAccount } from 'apis/transferApi';
+import { callAPI } from 'utilities/axios';
 
 /**
-   * 呼叫 API 新增登記帳戶
-   */
-const handleAdd = async (card) => {
-  const params = {
-    inBank: card?.bankId,
-    inAcct: card?.accountId,
-    nickName: card?.accountName,
-  };
-  try {
-    await addFavAccount(params);
-    return true;
-  } catch (error) {
-    // TODO: You may want to remove below line in production.
-    console.warn('Error returned from updateFavAccount', error);
-    return false;
-  }
+ * 查詢用戶自設的常用轉入帳號清單。
+ * @returns [{
+ *   bankId: 常用轉入帳戶-銀行代碼
+ *   acctId: 常用轉入帳戶-帳號
+ *   bankName: 銀行名稱 // TODO 目前應該是無值，因為MBGW沒看到！
+ *   nickName: 暱稱
+ *   email: 通知EMAIL
+ *   headshot: 大頭照，只有常用轉入帳戶是Bankee會員才會有值。若為 null 表示沒有頭像。
+ * }, ...]
+ */
+export const getAllFrequentAccount = async () => {
+  const response = await callAPI('/api/transfer/frequentAccount/v1/getAll');
+  return response.data;
 };
 
 /**
-   * 呼叫 API 更新帳戶資訊
-   */
-const handleEdit = async (card) => {
-  const params = {
-    email: card?.email,
-    inBank: card?.bankId,
-    inAcct: card?.accountId,
-    nickName: card?.accountName,
-    orgBankId: card?.bankId,
-    orgAcctId: card?.accountId,
-  };
-  try {
-    await updateFavAccount(params);
-    return true;
-  } catch (error) {
-    // TODO: You may want to remove below line in production.
-    console.warn('Error returned from updateFavAccount', error);
-    return false;
-  }
+ * 新增常用轉入帳號。
+ * @param {*} account {
+ *   bankId: 常用轉入帳戶-銀行代碼
+ *   acctId: 常用轉入帳戶-帳號
+ *   nickName: 新暱稱；可為空值
+ *   email: 新通知EMAIL；可為空值
+ * }
+ * @returns
+ */
+export const addFrequentAccount = async (account) => {
+  const response = await callAPI('/api/transfer/frequentAccount/v1/getAll', account);
+  return response.data;
 };
 
 /**
-   * 呼叫 API 移除登記帳戶
-   */
-const handleRemove = async (card) => {
-  const params = {
-    email: card?.email,
-    inBank: card?.bankId,
-    inAcct: card?.accountId,
-    nickName: card?.accountName,
-  };
-  try {
-    await removeFavAccount(params);
-    return true;
-  } catch (error) {
-    // TODO: You may want to remove below line in production.
-    console.warn('Error returned from removeFavAccount', error);
-    return false;
-  }
+ * 更新常用轉入帳號。
+ * @param {*} account {
+ *   bankId: 常用轉入帳戶-銀行代碼
+ *   acctId: 常用轉入帳戶-帳號
+ *   nickName: 新暱稱；可為空值
+ *   email: 新通知EMAIL；可為空值
+ *   orgBankId: 變更前 常用轉入帳戶-銀行代碼，未變更也需要有值。
+ *   orgAcctId: 變更前 常用轉入帳戶-帳號，未變更也需要有值。
+ * }
+ * @returns
+ */
+export const updateFrequentAccount = async (account) => {
+  const response = await callAPI('/api/transfer/frequentAccount/v1/getAll', account);
+  return response.data;
 };
 
-export { handleAdd, handleEdit, handleRemove };
+/**
+ * 刪除常用轉入帳號。
+ * @param {*} bankId 常用轉入帳戶-銀行代碼
+ * @param {*} acctId 常用轉入帳戶-帳號
+ * @returns
+ */
+export const deleteFrequentAccount = async (bankId, acctId) => {
+  const response = await callAPI('/api/transfer/frequentAccount/v1/delete', { bankId, acctId });
+  return response.data;
+};
