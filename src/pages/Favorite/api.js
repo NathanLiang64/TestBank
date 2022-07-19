d7e73236e27f2f67fd37714fb8bc584dfd752f21
brian <xxbrian1031@gmail.com>
2022-07-19 19:33:11 +0800

    增修欄位說明

--- src/pages/Favorite/api.js
+++ src/pages/Favorite/api.js
@@ -4,18 +4,18 @@ import { callAPI } from 'utilities/axios';
  * (我的最愛)取得提供設定的項目選單
  * @param JwtToken
  * @return{
- *   groupKey:
- *   groupName:
+ *   groupKey:  群組代碼
+ *   groupName: 群組名稱
  *   items {
- *      actKey:
- *      name:
- *      url:
- *      icon:
- *      groupType:
- *      alterMsg:
- *      isFavorite:
- *      position:
- *   }
+ *      actKey:     功能代碼
+ *      name:       功能名稱
+ *      url:        連結
+ *      icon:       圖示
+ *      groupType:  權限參數
+ *      alterMsg:   顯示用訊息
+ *      isFavorite: 是否已經設為我的最愛
+ *      position:   位置資訊
+ *   },...
  * }
  * @throws Exception
  */
@@ -27,14 +27,14 @@ export const getFavoriteSettingList = async () => {
  * (我的最愛)取得已設定的項目清單(最多12個)
  * @param JwtToken
  * @return{
- *   actKey:
- *   name:
- *   url:
- *   icon:
- *   groupType:
- *   alterMsg:
- *   isFavorite:
- *   position:
+ *   actKey:    功能代碼
+ *   name:      功能名稱
+ *   url:       連結
+ *   icon:      圖示
+ *   groupType: 權限參數
+ *   alterMsg:  顯示用訊息
+ *   isFavorite:是否已經設為我的最愛
+ *   position:  位置資訊
  * }
  * @throws Exception
  */
@@ -46,14 +46,14 @@ export const getFavoriteList = async () => {
  * (我的最愛)新增/更新已設定的項目清單
  * @param JwtToken
  * @param {
- *   actKey:"A03",
- *   position: 8,
- *   otherDesc: "1000"
+ *   actKey:    功能代碼
+ *   position:  位置資訊
+ *   otherDesc: 其他資訊
  *   }
  * @returns {
- *   result: "true/false"
- *   message:""
- *   actKey: "D00100"
+ *   result:  true/false
+ *   message: 回傳訊息
+ *   actKey:  功能代碼
  *   }
  * @throws Exception
  * 已存在清單中的項目更新位置，沒有的項目則新增
@@ -67,11 +67,11 @@ export const modifyFavoriteItem = async (request) => {
 * (我的最愛)刪除清單中的項目
 *
 * @param JwtToken
-* @param actKey: "D00100"
+* @param actKey: 功能代碼
 * @returns{
-*     result: "true/false"
-*     message:""
-*     actKey: "D00100"
+*     result:   true/false
+*     message:  回傳訊息
+*     actKey:   功能代碼
 *   }
 * ORDS API的刪除功能是用 [actKey=null + position的值] 來判斷要刪除的功能，這API轉成前端直接傳actKey來指定刪除的功能
 * @throws Exception
--- src/proto/Nav/Nav.api.js
+++ src/proto/Nav/Nav.api.js
@@ -39,3 +39,83 @@ export const functionTrace = async (request) => {
   const response = await callAPI('/smApi/v1/functionTrace', request);
   return response;
 };
+/**
+ * (我的最愛)取得提供設定的項目選單
+ * @param JwtToken
+ * @return{
+ *   groupKey:  群組代碼
+ *   groupName: 群組名稱
+ *   items {
+ *      actKey:     功能代碼
+ *      name:       功能名稱
+ *      url:        連結
+ *      icon:       圖示
+ *      groupType:  權限參數
+ *      alterMsg:   顯示用訊息
+ *      isFavorite: 是否已經設為我的最愛
+ *      position:   位置資訊
+ *   },...
+ * }
+ * @throws Exception
+ */
+export const getFavoriteSettingList = async () => {
+  const response = await callAPI('/api/menu/v1/favFuncList/getSetting');
+  return response.data;
+};
+/**
+ * (我的最愛)取得已設定的項目清單(最多12個)
+ * @param JwtToken
+ * @return{
+ *   actKey:    功能代碼
+ *   name:      功能名稱
+ *   url:       連結
+ *   icon:      圖示
+ *   groupType: 權限參數
+ *   alterMsg:  顯示用訊息
+ *   isFavorite:是否已經設為我的最愛
+ *   position:  位置資訊
+ * }
+ * @throws Exception
+ */
+export const getFavoriteList = async () => {
+  const response = await callAPI('/api/menu/v1/favFuncList/getAll');
+  return response.data;
+};
+/**
+ * (我的最愛)新增/更新已設定的項目清單
+ * @param JwtToken
+ * @param {
+ *   actKey:    功能代碼
+ *   position:  位置資訊
+ *   otherDesc: 其他資訊
+ *   }
+ * @returns {
+ *   result:  true/false
+ *   message: 回傳訊息
+ *   actKey:  功能代碼
+ *   }
+ * @throws Exception
+ * 已存在清單中的項目更新位置，沒有的項目則新增
+ * TODO: otherDesc尚未加入ORDS API中
+ */
+export const modifyFavoriteItem = async (request) => {
+  const response = await callAPI('/api/menu/v1/favFuncList/modify', request);
+  return response.data;
+};
+/**
+* (我的最愛)刪除清單中的項目
+*
+* @param JwtToken
+* @param actKey: 功能代碼
+* @returns{
+*     result:   true/false
+*     message:  回傳訊息
+*     actKey:   功能代碼
+*   }
+* ORDS API的刪除功能是用 [actKey=null + position的值] 來判斷要刪除的功能，這API轉成前端直接傳actKey來指定刪除的功能
+* @throws Exception
+*/
+export const deleteFavoriteItem = async (request) => {
+  const response = await callAPI('/api/menu/v1/favFuncList/delete', request);
+  return response.data;
+};
