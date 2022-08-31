43cdb07d05a1c47f967cca986308b9a354c0a692
brian <xxbrian1031@gmail.com>
2022-08-31 17:21:27 +0800

    移除沒用到的import

--- src/pages/D00800_ReserveTransferSearch/reserveTransferSearch_2.js
+++ src/pages/D00800_ReserveTransferSearch/reserveTransferSearch_2.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router';
 import { toCurrency } from 'utilities/Generator';
 
 /* Elements */
-import Header from 'components/Header';
+// import Header from 'components/Header';
 import { FEIBButton } from 'components/elements';
 import InformationList from 'components/InformationList';
 import Accordion from 'components/Accordion';
