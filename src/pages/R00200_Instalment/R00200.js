5e9e7864765ee64d6e8c099771997d737d9ffcbc
leetw131 <leetw@jgallop.com>
2022-09-28 17:05:25 +0800

    調整：R00200 選單改由yup, hook form控制驗證及送出資料

--- src/pages/R00200_Instalment/R00200.js
+++ src/pages/R00200_Instalment/R00200.js
@@ -3,6 +3,9 @@
 // import {useEffect} from 'react';
 import { useHistory } from 'react-router';
 import { useCheckLocation, usePageInfo } from 'hooks';
+import * as yup from 'yup';
+import { Controller, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 /* Elements */
 import Layout from 'components/Layout/Layout';
@@ -18,7 +21,7 @@ import R00200AccordionContent2 from './R00200_accordionContent_2';
 import InstalmentWrapper from './R00200.style';
 
 /**
- * R00200 晚點付手頁
+ * R00200 晚點付首頁
  */
 
 const R00200 = () => {
@@ -29,8 +32,6 @@ const R00200 = () => {
   // const cardName = 'cardName';
 
   /* showError 內容: 單筆消費未達3000 */
-  // const errorMessage = `您目前沒有可分期的消費
-  // (單筆消費限額需達3,000元以上)`;
   const errorMessage = (
     <div style={{ textAlign: 'center' }}>
       <p>您目前沒有可分期的消費</p>
@@ -42,19 +43,34 @@ const R00200 = () => {
   usePageInfo('/api/instalment');
   const history = useHistory();
 
-  // TODO: 增加form驗證
+  /* 資料驗證 */
+  const schema = yup.object().shape({
+    installmentType: yup.string().required('請選擇欲申請之晚點付項目'),
+  });
+  const { handleSubmit, control } = useForm({
+    resolver: yupResolver(schema),
+  });
+
   const renderSelectList = () => {
     const list = ['單筆', '總額'];
     return (
       <div className="selectList">
-        <RadioGroup
-          id="installmentType"
+        <Controller
           name="installmentType"
+          control={control}
           defaultValue="1"
-        >
-          <FEIBRadioLabel value="1" control={<FEIBRadio />} label={list[0]} />
-          <FEIBRadioLabel value="2" control={<FEIBRadio />} label={list[1]} />
-        </RadioGroup>
+          render={({field}) => (
+            <RadioGroup
+              {...field}
+              id="installmentType"
+              name="installmentType"
+              defaultValue="1"
+            >
+              <FEIBRadioLabel value="1" control={<FEIBRadio />} label={list[0]} />
+              <FEIBRadioLabel value="2" control={<FEIBRadio />} label={list[1]} />
+            </RadioGroup>
+          )}
+        />
       </div>
     );
   };
@@ -81,10 +97,18 @@ const R00200 = () => {
   //   />
   // );
 
+  const handleOnSubmit = (data) => {
+    console.log('R00200 handleOnSubmit() data: ', data);
+
+    // TODO: 是否在此檢查有無符合條件，有則push、無則showError
+    showError(errorMessage);
+    history.push('/R002001'); // TODO: 需帶參數進下一頁
+  };
+
   return (
     <Layout title="晚點付">
       <InstalmentWrapper className="InstalmentWrapper" small>
-        <form onSubmit={() => { }}>
+        <form onSubmit={handleSubmit((data) => handleOnSubmit(data))}>
           <div>
             <div className="InstalmentWrapperText">點選申請晚點付項目</div>
             {renderSelectList()}
@@ -92,14 +116,7 @@ const R00200 = () => {
               <R00200AccordionContent2 />
             </Accordion>
           </div>
-          <FEIBButton
-            type="submit"
-            onClick={() => {
-              // TODO: 是否在此檢查有無符合條件？（有則push(依照條件)、無則showError）
-              showError(errorMessage);
-              history.push('/R002001'); // TODO: 未帶參數進下一頁
-            }}
-          >
+          <FEIBButton type="submit">
             下一步
           </FEIBButton>
           {/* TODO: 移除 */}
