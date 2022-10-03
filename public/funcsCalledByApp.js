e424add31cc11efc0cd51afeaa174e0def8280a6
Jerry Yeh <jerry88634@feib.com.tw>
2022-10-03 09:46:21 +0800

    修正：AppScriptProxy 在非同步多執行緒模式下，只用單一Promise所衍生的錯誤。

--- public/funcsCalledByApp.js
+++ public/funcsCalledByApp.js
@@ -1,24 +1,7 @@
 /**
- * 負責接收 APP JavaScript API callback 的共用方法。
- * @param {*} value APP JavaScript API的傳回值。
- */
-/* eslint-disable */
-function AppJavaScriptCallback(value) {
-  // console.log('*** Result from APP JavaScript : ', value);  
-  let result;
-  try {
-    // 若是 JSON 格式，則以物件型態傳回。
-    result = JSON.parse(value);
-  } catch (ex) {
-    result = value;
-  }
-  // AppJavaScriptCallbackPromiseResolve 是在 utility/AppScriptProxy.js 的 callAppJavaScript() 方法中定義。
-  window.AppJavaScriptCallbackPromiseResolve(result);
-}
-
-/**
  * APP呼叫，通知WebView返回功能上一頁
  */
+// eslint-disable-next-line no-unused-vars
 function goBack() {
   // TODO 如何通知執行中的 WebView ？
 }
