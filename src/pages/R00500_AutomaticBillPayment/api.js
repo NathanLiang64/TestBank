ddd8bd457519cf6ae25fdb8a67c8b630bb84dfd5
brian <xxbrian1031@gmail.com>
2022-10-13 15:50:54 +0800

    增加 API 說明: 1. 查詢客戶信用卡自動扣繳資訊 API: /api/card/v1/getAutoDebits 2. 申請信用卡自動扣繳 API: /api/card/v1/setAutoDebit

---
+++ src/pages/R00500_AutomaticBillPayment/api.js
@@ -0,0 +1,40 @@
+import { callAPI } from 'utilities/axios';
+
+/**
+ * 查詢客戶信用卡自動扣繳資訊
+ * (畫面_信用卡子首頁_自動扣繳 - 1)
+ *
+ * @param token
+ * @return {
+ *    bank      銀行別                                     IVR9019.DEDUCT-BANK
+ *    account   扣繳帳號                                   IVR9019.DEDUCT-ACCOUNT-NO
+ *    isFullPay 是否指定應繳總額 Y/N                        IVR9019.DEDUCT-AUTOPAY-RATE=100 ? "Y":"N"
+ *    status    狀態 1 申請 2 生效 3 取消 4 退件 5 待生效   IVR9019.DEDUCT-STATUS
+ * }
+ *
+ * TODO: 尚未有完整的測資，accountId 先固定帶 A123014281
+ */
+export const getAutoDebits = async (request) => {
+  const response = await callAPI('/api/card/v1/getAutoDebits', request);
+  return response;
+};
+
+/**
+ * 申請信用卡自動扣繳
+ *
+ * @param token
+ * @param {
+ *    bank:       指定銀行代碼
+ *    account:    扣繳帳號
+ *    isFullPay:  是否指定應繳總額 Y/N
+ * }
+ * @return {
+ *    result:     true/false
+ *    message:    回傳結果
+ * }
+ *
+ */
+export const setAutoDebit = async (request) => {
+  const response = await callAPI('/api/card/v1/setAutoDebit', request);
+  return response;
+};
