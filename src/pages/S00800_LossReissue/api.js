ba669096f6c600656d90cc0128df84d5006cd0d7
brian <xxbrian1031@gmail.com>
2022-10-31 18:44:31 +0800

    增修說明: /v1/getStatus 增加 account 回傳參數

--- src/pages/S00800_LossReissue/api.js
+++ src/pages/S00800_LossReissue/api.js
@@ -30,6 +30,7 @@ import { callAPI } from 'utilities/axios';
  *    addrCity:     通訊地址(縣市)    g0101.ADR2-CT
  *    addrDistrict: 通訊地址(區)      g0101.ADR2-AR
  *    addrStreet:   通訊地址(街道路)   g0101.ADR2-RD
+ *    account:      台幣數存母帳號
  * }
  * @throws Exception
  *
