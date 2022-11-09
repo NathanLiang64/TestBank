e33dd5f618a211489880772e7cf82408c0cab4cb
邱韋中 <qiuweizhong@qiuweizhongdeMacBook-Pro.local>
2022-11-09 19:02:13 +0800

    調整: S00700 金融卡掛失/補發 - 1.新增交易授權認證 2.新增縣市清單 API 3.調整呈表單之 Modal 樣式

--- src/pages/S00800_LossReissue/S00800.js
+++ src/pages/S00800_LossReissue/S00800.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -5,39 +6,49 @@ import Layout from 'components/Layout/Layout';
 import Accordion from 'components/Accordion';
 import SuccessFailureAnimations from 'components/SuccessFailureAnimations';
 import { DropdownField, TextInputField } from 'components/Fields';
-import { FEIBButton } from 'components/elements';
+import { FEIBButton, FEIBInputLabel } from 'components/elements';
 import { EditIcon } from 'assets/images/icons';
-import { showCustomPrompt } from 'utilities/MessageModal';
-import { closeFunc } from 'utilities/AppScriptProxy';
+import { showCustomDrawer, showCustomPrompt, showError } from 'utilities/MessageModal';
+import { closeFunc, transactionAuth } from 'utilities/AppScriptProxy';
 import { useDispatch } from 'react-redux';
-import { setModalVisible } from 'stores/reducers/ModalReducer';
+import { setDrawerVisible, setModalVisible } from 'stores/reducers/ModalReducer';
 
+import { getCountyList } from 'pages/T00700_BasicInformation/api';
 import {getStatus, reIssueOrLost} from './api';
-import LossReissueWrapper from './lossReissue.style';
-import {
-  actionTextGenerator, cityOptions, zoneOptions,
-} from './utils';
-import { generateSchema } from './validationSchema';
+import LossReissueWrapper, {
+  LossReissueDialogWrapper,
+} from './lossReissue.style';
+import {actionTextGenerator} from './utils';
+import { validationSchema } from './validationSchema';
 
 const LossReissue = () => {
   const dispatch = useDispatch();
   const [debitCardInfo, setDebitCardInfo] = useState();
   const [currentFormValue, setCurrentFormValue] = useState({});
-  const actionText = useMemo(() => actionTextGenerator(debitCardInfo?.status), [debitCardInfo]);
-  const { control, handleSubmit, reset } = useForm({
-    resolver: yupResolver(generateSchema(actionText)),
+  const [countyOptions, setCountyOptions] = useState([]);
+  const actionText = useMemo(
+    () => actionTextGenerator(debitCardInfo?.status),
+    [debitCardInfo],
+  );
+  const {
+    control, handleSubmit, reset, watch, getValues,
+  } = useForm({
+    resolver: yupResolver(validationSchema),
     defaultValues: {
       // ...目前尚未得知補發 api 需要帶哪些資訊，先預設 addrDistrict
-      addrDistrict: '',
-      addrCity: '',
-      addrStreet: '',
+      city: '',
+      county: '',
+      addr: '',
     },
   });
+
+  const watchedCountyName = watch('county');
 
   // 在 新申請(1) 或是 已銷戶(7) 的情況下不能進行掛失或補發
   const updateDebitCardStatus = async () => {
     const cardInfo = await getStatus();
     if (cardInfo) {
+      // "新申請" 或是 "已銷戶" 狀態下不可進行掛失/補發
       if (cardInfo.status === 1 || cardInfo.status === 7) {
         await showCustomPrompt({
           message: cardInfo.statusDesc,
@@ -46,114 +57,137 @@ const LossReissue = () => {
         });
       }
 
+      const formValue = {
+        county: cardInfo.addrCity.trim(),
+        city: cardInfo.addrDistrict.trim(),
+        addr: cardInfo.addrStreet,
+      };
+
+      reset(formValue);
       setDebitCardInfo(cardInfo);
-      const { addrCity, addrDistrict, addrStreet } = cardInfo;
-      reset({ addrCity, addrDistrict, addrStreet });
-      setCurrentFormValue({ addrCity, addrDistrict, addrStreet });
+      setCurrentFormValue(formValue);
     }
   };
 
   // 執行掛失或補發
-  const executeAction = async (values) => {
-    const res = await reIssueOrLost(values);
-    showCustomPrompt({
-      message: (
-        <SuccessFailureAnimations
-          isSuccess={res && res.result}
-          successTitle={`${actionText}設定成功`}
-          errorTitle={`${actionText}設定失敗`}
-          successDesc={`狀態： (${debitCardInfo?.account}) ${res.message}`}
-          errorDesc={`狀態： (${debitCardInfo?.account}) ${res.message}`}
-        />
-      ),
-      onOk: () => updateDebitCardStatus(),
-      onclose: () => updateDebitCardStatus(),
-    });
+  const executeAction = async () => {
+    const auth = await transactionAuth(0x26);
+    if (auth && auth.result) {
+      const res = await reIssueOrLost();
+      showCustomPrompt({
+        message: (
+          <SuccessFailureAnimations
+            isSuccess={res && res.result}
+            successTitle={`${actionText}設定成功`}
+            errorTitle={`${actionText}設定失敗`}
+            successDesc={`狀態： (${debitCardInfo?.account}) ${res.message}`}
+            errorDesc={`狀態： (${debitCardInfo?.account}) ${res.message}`}
+          />
+        ),
+        onOk: () => updateDebitCardStatus(),
+        onclose: () => updateDebitCardStatus(),
+      });
+    }
   };
 
-  const handleClickSubmitButton = (values) => {
+  const onActionConfirm = () => {
     showCustomPrompt({
       message: `是否確認${actionText}?`,
-      onOk: () => executeAction(values),
+      onOk: () => executeAction(),
       noDismiss: true,
     });
   };
 
+  const onSubmit = async (values) => {
+    const auth = await transactionAuth(0x26);
+    if (auth && auth.result) {
+      // TODO 修改地址 API
+      console.log(values);
+    }
+
+    dispatch(setDrawerVisible(false));
+  };
+
+  const generateCountyOptions = () => {
+    if (countyOptions.length) {
+      return countyOptions.map(({ countyName }) => ({
+        label: countyName,
+        value: countyName,
+      }));
+    }
+    return [];
+  };
+  // 建立鄉鎮市區選單
+  const generateDistrictOptions = () => {
+    const foundDistrictOption = countyOptions.find(
+      ({ countyName }) => countyName === watchedCountyName,
+    );
+    if (foundDistrictOption) {
+      return foundDistrictOption.cities.map(({ cityName }) => ({
+        label: cityName,
+        value: cityName,
+      }));
+    }
+    return [];
+  };
   // TODO: webctl 地址欄位 api 待修正，現為 1 欄字串，待修為改城市、行政區、詳細地址 3 欄
   const renderEditAddressContent = () => (
-    <form>
-      <div className="formContent">
-        <div className="formElementGroup">
-          <div>
-            <DropdownField
-              labelName="縣市"
-              options={cityOptions}
-              name="addrCity"
-              control={control}
-            />
+    <LossReissueDialogWrapper>
+      <form>
+        <div className="formContent">
+          <FEIBInputLabel>通訊地址</FEIBInputLabel>
+          <div className="formElementGroup">
+            <div>
+              <DropdownField
+                options={generateCountyOptions()}
+                name="county"
+                control={control}
+              />
+            </div>
+            <div>
+              <DropdownField
+                options={generateDistrictOptions()}
+                name="city"
+                control={control}
+              />
+            </div>
           </div>
           <div>
-            <DropdownField
-              labelName="區域"
-              options={zoneOptions}
-              name="addrDistrict"
-              control={control}
-            />
+            <TextInputField labelName="地址" name="addr" control={control} />
           </div>
         </div>
-        <div>
-          <TextInputField
-            labelName="地址"
-            name="addrStreet"
-            control={control}
-          />
-        </div>
-      </div>
-    </form>
+        <FEIBButton onClick={handleSubmit(onSubmit)}>
+          確認
+        </FEIBButton>
+      </form>
+    </LossReissueDialogWrapper>
   );
 
   const handleClickEditAddress = () => {
-    showCustomPrompt({
+    showCustomDrawer({
       title: '通訊地址',
-      message: renderEditAddressContent(),
+      content: renderEditAddressContent(),
       okContent: '完成',
-      onOk: handleSubmit((values) => {
-        setCurrentFormValue(values);
-        dispatch(setModalVisible(false));
-      }),
-      onClose: () => {
-        reset(currentFormValue);
-      },
-      noDismiss: true,
+      onClose: () => reset(currentFormValue),
     });
   };
 
   useEffect(async () => {
-    updateDebitCardStatus();
+    const listResponse = await getCountyList();
+    if (listResponse.code === '0000') {
+      setCountyOptions(listResponse.data);
+      updateDebitCardStatus();
+    } else {
+      showError(listResponse.message, closeFunc);
+    }
   }, []);
 
-  // useEffect(() => {
-  //   if (debitCardInfo?.cardStatus === '01') {
-  //     showCustomPrompt({
-  //       message: '卡片狀態:新申請',
-  //       onOk: () => closeFunc(),
-  //       onClose: () => closeFunc(),
-  //     });
-  //   } else {
-  //     const action = actionTextGenerator(debitCardInfo?.cardStatus);
-  //     setActionText(action);
-  //   }
-  // }, [debitCardInfo?.cardStatus]);
-
-  // 提供 react-hook-form 預設值
-  // useEffect(() => {
-  //   if (debitCardInfo) {
-  //     const { addrDistrict, addrCity, addrStreet } = debitCardInfo;
-  //     const defaultValues = { addrDistrict, addrCity, addrStreet };
-  //     reset(defaultValues);
-  //     setCurrentFormValue(defaultValues);
-  //   }
-  // }, [debitCardInfo]);
+  // 當使用者改變 "county" 值時，需要將 "city" 值清空
+  useEffect(() => {
+    if (watchedCountyName !== currentFormValue.county) {
+      reset({ ...getValues(), city: '' });
+    }
+  }, [watchedCountyName]);
 
   return (
     <Layout title="金融卡掛失/補發">
@@ -169,19 +203,21 @@ const LossReissue = () => {
                 <h3 className="debitState">{debitCardInfo?.statusDesc}</h3>
               </div>
             </li>
-            {actionText === '補發' && (
-              <li>
-                <div className="blockLeft">
-                  <p className="label">通訊地址</p>
-                  <span className="content">{currentFormValue.addrStreet || '-'}</span>
-                </div>
-                <div className="blockRight">
-                  <button type="button" onClick={handleClickEditAddress}>
-                    <EditIcon />
-                  </button>
-                </div>
-              </li>
-            )}
+            {/* {actionText === '補發' && ( */}
+            <li>
+              <div className="blockLeft">
+                <p className="label">通訊地址</p>
+                <span className="content">
+                  {currentFormValue.addrStreet || '-'}
+                </span>
+              </div>
+              <div className="blockRight">
+                <button type="button" onClick={handleClickEditAddress}>
+                  <EditIcon />
+                </button>
+              </div>
+            </li>
+            {/* )} */}
           </ul>
 
           {actionText === '補發' && (
@@ -194,17 +230,25 @@ const LossReissue = () => {
 
           <Accordion space="top">
             <ol>
-              <li>Bankee存款帳戶申請補發Bankee金融卡，手續費新臺幣(以下同)100元及郵寄掛號費50元將由Bankee存款帳戶中自動扣除(前述Bankee存款帳戶泛指持有「Bankee數位存款帳戶」或「Bankee一般帳戶」者，以下簡稱本存戶)。</li>
-              <li>本存戶向遠東國際商業銀行辦理金融卡申請/異動申請，除金融卡註銷外，嗣後往來仍悉遵「遠東國際商業銀行金融卡服務約定事項」有關業務規定辦理。</li>
-              <li>於各項異動手續辦理妥前，所有使用本存戶Bankee金融卡之交易或申請人為不實之申請，而致蒙受損害時，其一切損害及責任概由本存戶負責。</li>
-              <li>本存戶於申請此服務時，業已審閱並充分了解全部內容，並完全同意後才使用各項服務及申請憑證。</li>
+              <li>
+                Bankee存款帳戶申請補發Bankee金融卡，手續費新臺幣(以下同)100元及郵寄掛號費50元將由Bankee存款帳戶中自動扣除(前述Bankee存款帳戶泛指持有「Bankee數位存款帳戶」或「Bankee一般帳戶」者，以下簡稱本存戶)。
+              </li>
+              <li>
+                本存戶向遠東國際商業銀行辦理金融卡申請/異動申請，除金融卡註銷外，嗣後往來仍悉遵「遠東國際商業銀行金融卡服務約定事項」有關業務規定辦理。
+              </li>
+              <li>
+                於各項異動手續辦理妥前，所有使用本存戶Bankee金融卡之交易或申請人為不實之申請，而致蒙受損害時，其一切損害及責任概由本存戶負責。
+              </li>
+              <li>
+                本存戶於申請此服務時，業已審閱並充分了解全部內容，並完全同意後才使用各項服務及申請憑證。
+              </li>
             </ol>
           </Accordion>
         </div>
 
         {actionText && (
-          <FEIBButton onClick={handleSubmit(handleClickSubmitButton)}>
-            {`${actionText}申請`}
+          <FEIBButton onClick={onActionConfirm}>
+            {`${actionText}`}
           </FEIBButton>
         )}
       </LossReissueWrapper>
--- src/pages/S00800_LossReissue/validationSchema.js
+++ src/pages/S00800_LossReissue/validationSchema.js
@@ -3,17 +3,20 @@ import { addressValidation } from 'utilities/validation';
 import * as yup from 'yup';
 
 export const validationSchema = yup.object().shape({
-  addr: addressValidation(),
+  // addr: addressValidation(),
+  county: yup.string().required('請選擇縣市'),
+  city: yup.string().required('請選擇鄉鎮市區'),
+  addr: yup.string().required('請輸入通訊地址'),
 });
 
-export const generateSchema = (actionText) => {
-  let schema;
-  if (actionText === '補發') {
-    schema = yup.object().shape({
-      addrStreet: addressValidation(),
-    });
-  } else {
-    schema = yup.object().shape({});
-  }
-  return schema;
-};
+// const generateSchema = (actionText) => {
+//   let schema;
+//   if (actionText === '補發') {
+//     schema = yup.object().shape({
+//       addrStreet: addressValidation(),
+//     });
+//   } else {
+//     schema = yup.object().shape({});
+//   }
+//   return schema;
+// };
