49132fd236eb862aef83679f73ce3967654daefb
leetw131 <leetw@jgallop.com>
2022-11-11 11:47:20 +0800

    新增：FuncID.js 統整funcID成全域變數 新增：TxnAuthCode.js 統整authCode成全域變數

---
+++ src/utilities/TxnAuthCode.js
@@ -0,0 +1,37 @@
+export const A00700 = 0x28; // 定期更新密碼(一年)
+
+export const C00600 = 0x30; // 新增/關閉存錢計畫(約轉、預約轉)
+export const C00900 = 0x30; // 台幣定存
+export const C01000 = 0x30; // 外幣定存
+
+export const D00100_NONREG = 0x20; // 台幣-非約轉帳
+export const D00100_REG = 0x30; // 台幣-約定轉帳
+export const D00300 = 0x20; // 無卡提款-交易
+export const D00400 = 0x20; // 無卡提款密碼變更流程
+export const D00700 = 0x30; // 本行同幣別外幣轉帳
+export const D00800 = 0x28; // 預約轉帳取消
+
+export const E00100_TWD_F = 0x30; // 換匯-台換外
+export const E00100_F_TWD = 0x30; // 換匯-外換台
+
+export const R00200 = 0x30; // 信用卡-消費分期
+export const R00400 = 0x30; // 信用卡-繳費
+export const R00500 = 0x30; // 信用卡-自動扣繳
+export const R00600 = 0x30; // 信用卡-額度臨調
+
+export const S00400 = 0x30; // 訊息通知設定(第一次開啟)
+export const S00700 = 0x20; // 金融卡啟用
+export const S00800 = 0x28; // 金融卡掛失補發
+
+export const T00200_SET = 0x11; // 生物辨識/圖形-設定
+export const T00200_UNSET = 0x20; // 生物辨識/圖形-解除
+export const T00300_APPLY = 0x20; // 非約轉設定-申請
+export const T00300_EDIT = 0x21; // 非約轉設定-手機號碼修改
+export const T00300_CLOSE = 0x20; // 非約轉設定-關閉
+export const T00400 = 0x20; // 無卡提款-設定
+export const T00600 = 0x23; // 手機號碼收款設定
+export const T00700_MOBILE = 0x31; // 基本資料變更-通訊門號
+export const T00700_EMAIL = 0x28; // 基本資料變更-電子郵件
+export const T00700_ADDRESS = 0x28; // 基本資料變更-通訊地址
+export const T00800 = 0x28; // 使用者代號變更
+export const T00900 = 0x28; // 網銀密碼變更
