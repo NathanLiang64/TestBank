bae5c93b7e0df0d53ab9b09eb63273424f877733
邱韋中 <qiuweizhong@qiuweizhongdeMacBook-Pro.local>
2022-11-14 15:25:39 +0800

    調整: S00700 金融卡啟用 - 金融卡啟用完成頁面之版面調整

--- src/pages/S00700_DebitCardActive/S00700.style.js
+++ src/pages/S00700_DebitCardActive/S00700.style.js
@@ -9,13 +9,11 @@ export default DebitCardActiveWrapper;
 
 export const SuccessDescWrapper = styled.div`
     text-align:left;
-    font-weight: 600;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    margin: 0 auto;
-    width: 85%;
+    margin: 2rem auto;
 
     & >div{
         margin-bottom:1rem;
