8527fcfb45e6918ac945b2be27d484a4fe595d31
邱韋中 <qiuweizhong@qiuweizhongdeMacBook-Pro.local>
2022-11-14 18:07:33 +0800

    新增 useLocationOptions hook，後續需要縣市鄉鎮選單可呼叫此 hook 拿取

---
+++ src/hooks/useLocationOptions.js
@@ -0,0 +1,43 @@
+import { useEffect, useState, useMemo } from 'react';
+import { getCountyList } from 'pages/T00700_BasicInformation/api';
+
+export const useLocationOptions = (watchedCountyName) => {
+  const [locationLists, setLocationLists] = useState([]);
+
+  // 縣市選單
+  const countyOptions = useMemo(() => {
+    if (locationLists.length) {
+      return locationLists.map(({ countyName }) => ({
+        label: countyName,
+        value: countyName,
+      }));
+    }
+    return [];
+  }, [locationLists]);
+
+  // 建立鄉鎮市區選單
+  const districtOptions = useMemo(() => {
+    const foundDistrictOption = locationLists.find(
+      ({ countyName }) => countyName === watchedCountyName,
+    );
+    if (foundDistrictOption) {
+      return foundDistrictOption.cities.map(({ cityName }) => ({
+        label: cityName,
+        value: cityName,
+      }));
+    }
+    return [];
+  }, [watchedCountyName, locationLists]);
+
+  useEffect(async () => {
+    const { code, data } = await getCountyList({});
+    if (code === '0000') {
+      setLocationLists(data);
+    }
+  }, []);
+
+  return {
+    countyOptions,
+    districtOptions,
+  };
+};
