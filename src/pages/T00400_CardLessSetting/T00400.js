1df99f15e846bf388cc28ed05d6e70452d9e7316
邱韋中 <qiuweizhong@qiuweizhongdeMacBook-Pro.local>
2022-11-21 16:27:52 +0800

    修正: T00400 無卡提款設定 - 1. 切換 switch 時應依照不同卡況進行頁面跳轉或是 api request

--- src/pages/T00400_CardLessSetting/T00400.js
+++ src/pages/T00400_CardLessSetting/T00400.js
@@ -15,73 +15,54 @@ import { showError } from 'utilities/MessageModal';
 import CardLessSettingWrapper from './T00400.style';
 
 import { getStatus, activate } from './api';
+import { checkQLStatus } from './utils';
 
 const CardLessSetting = () => {
   const dispatch = useDispatch();
   const history = useHistory();
-  const [active, setActive] = useState(false);
+  const [cardLessStatus, setCardLessStatus] = useState();
   const [bankAccount, setBankAccount] = useState('');
-
-  const checkQLStatus = (status) => {
-    switch (status) {
-      case ('0'):
-        showError('裝置未綁定');
-        return false;
-      case ('3'):
-        showError('已在其它裝置綁定');
-        return false;
-      case ('4'):
-        showError('本裝置已綁定其他帳號');
-        return false;
-      default:
-        return true;
-    }
-  };
+  const isEnable = cardLessStatus === 2;
 
   // 點擊 switch 檢查裝置綁定狀態
-  const handleSwitchClick = async (e) => {
+  const handleSwitchClick = async () => {
     // 1. 確認裝置綁定狀態
     const { QLStatus } = await getQLStatus();
-    const shouldContinue = checkQLStatus(QLStatus);
+    const shouldContinue = await checkQLStatus(QLStatus);
+
     if (shouldContinue) {
-      // 2. 確認交易驗證
-      const {result} = await transactionAuth(AuthCode.T00400);
-      if (result) {
-        if (e.target.checked) {
-          // 開啟無卡提款
-          history.push('/T004001'); // 檢查成功跳轉設定無卡提款密碼頁
-        } else {
-          // 關閉無卡提款
-          const data = await activate();
-          if (data) setActive(false);
+      if (cardLessStatus === 0 || cardLessStatus === 3 || cardLessStatus === 4) {
+        // 跳轉設定無卡提款密碼頁
+        history.push('/T004001');
+      } else {
+        // 若是 1.已申請未開通 或是 2.已開通 狀態時需要先進行交易驗證
+        const { result } = await transactionAuth(AuthCode.T00400);
+        if (result) {
+          const { message } = await activate();
+          // 無 message 出現代表成功執行
+          if (!message) {
+            setCardLessStatus((prevStatus) => (prevStatus === 2 ? 3 : 2));
+          } else {
+            showError(message);
+          }
         }
       }
     }
   };
 
-  // 跳轉變更無卡提款密碼變更
-  const toChangePwd = () => {
-    startFunc(FuncID.D00400);
-  };
-
   // 檢查無卡提款狀態
-  const getCardlessStatus = async () => {
+  useEffect(async () => {
     dispatch(setWaittingVisible(true));
-    const response = await getStatus();
-    if (response.code === '0000') {
-      const { cwdStatus, account } = response.data;
-      const statusNumber = Number(cwdStatus);
-
-      if (statusNumber === 2) {
-        setBankAccount(account);
-        setActive(true);
-      }
+    try {
+      const {data} = await getStatus();
+      const statusNumber = Number(data.cwdStatus);
+      setBankAccount(data.account);
+      setCardLessStatus(statusNumber);
+    } catch (err) {
+      console.log('getStatus error', err.mesage);
     }
-    dispatch(setWaittingVisible(false));
-  };
 
-  useEffect(() => {
-    getCardlessStatus();
+    dispatch(setWaittingVisible(false));
   }, []);
 
   return (
@@ -91,30 +72,19 @@ const CardLessSetting = () => {
           <div className="switchContainer">
             <div className="labelContainer">
               <p className="labelTxt">無卡提款</p>
-              {
-                active && (
-                  <p className="phoneNum">{ bankAccount }</p>
-                )
-              }
+              {isEnable && <p className="phoneNum">{bankAccount}</p>}
             </div>
-            <FEIBSwitch
-              checked={active}
-              onClick={handleSwitchClick}
-            />
+            <FEIBSwitch checked={isEnable} onClick={handleSwitchClick} />
           </div>
-          {
-            active && (
-              <div className="mainBlock toChangePwd" onClick={toChangePwd}>
-                變更無卡提款密碼
-                <EditIcon />
-              </div>
-            )
-          }
+          {isEnable && (
+            <div className="mainBlock toChangePwd" onClick={() => startFunc(FuncID.D00400)}>
+              變更無卡提款密碼
+              <EditIcon />
+            </div>
+          )}
         </div>
         <Accordion title="使用條款" space="both">
-          <div className="dealContent">
-            無卡提款設定服務條款
-          </div>
+          <div className="dealContent">無卡提款設定服務條款</div>
         </Accordion>
       </CardLessSettingWrapper>
     </Layout>
