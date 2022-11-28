87186735fbec0efd8ac7f15adbc4e6445222eb7a
leetw131 <leetw@jgallop.com>
2022-11-28 10:59:06 +0800

    #651 調整：R00300 Reminder 下載方式

--- src/pages/R00300_CCBill/components/Reminder.jsx
+++ src/pages/R00300_CCBill/components/Reminder.jsx
@@ -32,14 +32,16 @@ const Reminder = ({ bills }) => {
       'END:VCALENDAR',
     ].join('\r\n');
     const blob = new Blob([context], { type: 'text/calendar;charset=utf-8' });
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = '提醒繳款.ics';
-    link.setAttribute('download', '提醒繳款.ics');
-    link.target = '_blank';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+
+    window.open(URL.createObjectURL(blob), '_blank');
+    // const link = document.createElement('a');
+    // link.href = URL.createObjectURL(blob);
+    // link.download = '提醒繳款.ics';
+    // link.setAttribute('download', '提醒繳款.ics');
+    // link.target = '_blank';
+    // document.body.appendChild(link);
+    // link.click();
+    // document.body.removeChild(link);
 
     URL.revokeObjectURL(blob);
   };
