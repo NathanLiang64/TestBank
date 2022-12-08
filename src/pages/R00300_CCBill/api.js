f7f89f753debc9f926a5a7ec62831238653254f4
leetw131 <leetw@jgallop.com>
2022-12-08 16:43:16 +0800

    調整：R00300 移除mock資料、調整api呼叫

--- src/pages/R00300_CCBill/api.js
+++ src/pages/R00300_CCBill/api.js
@@ -1,126 +1,76 @@
 import { callAPI } from 'utilities/axios';
-
 import { showCustomPrompt } from 'utilities/MessageModal';
-// import mockBills from './mockData/mockBills';
-// import mockBillDetails from './mockData/mockBillDetails';
-// import mockTransactions from './mockData/mockTransactions';
-import mockCreditCardTerms from './mockData/mockCreditCardTerms';
 
 /**
- * 取得信用卡繳費單
-   @param {
-     "period": 期別 (ex: 202207) (queryCardBill, getBillDetail 需要)
-   }
-   @returns {
-     "month": 本期月份。 // period末兩位數
-     "amount": 本期應繳金額，無帳單時為0。 // queryCardBillRt.newBalance
-     "billDate": 繳費截止日 // queryCardInfoRt.payDueDate
-     "currency": 幣值 // 'NTD'
-     "autoDeduct": 是否已設定自動扣繳 // checkCardBillStatusRt.autoDeductStatus
-     "hintToPay": 繳費提示文字 // checkCardBillStatusRt.hintToPay
-   }
+ * 查詢客戶信用卡帳單資訊
+ *
+ * @author danny
+ * @Date 2022/09/27
+ * @param token
+ * @param period 期別 (ex: 202212)
+ * }
+ * @return {
+ *    NewBalance,
+ *    Details [
+ *      {
+ *        txDate,   交易日期時間 (yyyymmddhhmmss)
+ *        cardNo,   卡號
+ *        desc,     消費項目說明
+ *        amount,   消費金額
+ *      },
+ *      ...
+ *    ]
+ * }
  */
-export const getBills = async (param) => {
-  // const response = await callAPI('/api/', param);
-  // const response = await new Promise((resolve) => resolve({ data: mockBills(param) }));
-
-  /* 自不同API取得data */
-  const queryCardBillRt = await callAPI('/api/card/v1/getBillSummary', param);
-  const checkCardBillStatusRt = await callAPI('/api/card/v1/getBillDeducStatus');
-  const billDetail = await callAPI('/api/card/v1/getBillDetail', param);
-
-  /* 將回傳資料轉換成頁面資料結構 */
-  const bills = {
-    month: param, // 只顯示月份，開頭不為0
-    amount: queryCardBillRt.data.newBalance,
-    billDate: billDetail.data.payDueDate,
-    currency: 'NTD',
-    autoDeduct: checkCardBillStatusRt.data.autoDeductStatus,
-    hintToPay: checkCardBillStatusRt.data.hintToPay,
-  };
-  return bills;
-  // return response.data;
+export const getBillSummary = async (request) => {
+  const response = await callAPI('/api/card/v1/getBillSummary', request);
+  return response.data;
 };
 
 /**
- * 取得信用卡交易明細：白底card，在 <Transactions /> 呼叫
-   @param {
-    "period": 期別 (ex: 202207) (僅 queryCardBill 需要)
-  }
-   @returns {
-     "txnDate": "20220425", // queryCardBillRt.detail[n].txDate
-     "description": "全家便利商店", // queryCardBillRt.detail[n].desc
-     "targetAcct": 卡號 // queryCardBillRt.detail[n].cardNo
-     "amount": 36000, // queryCardBillRt.detail[n].amount
-     "currency": "NTD"
-   }
+ * 信用卡帳單交易資訊
+ * (信用卡子首頁_帳單_更多)
+ *
+ * @param token
+ * @param period 期別 (ex: 202212)
+ * }
+ * @return {
+ *    newBalance              本期應繳總額      ORDS.stmt_amt
+ *    minDueAmount            最低應繳總額      ORDS.min_amt
+ *    billClosingDate         帳單結帳日        ORDS.stmt_date
+ *    payDueDate              繳款截止日        ORDS.pymt_due_date
+ *    prevBalance             上期應繳金額      ORDS.pre_pymt_due_amt
+ *    paidRefundAmount        已繳款/退款金額    ORDS.paid_amt
+ *    newPurchaseAmount       本期新增款項      ORDS.new_stmt_amt
+ *    interestFee             利息              ORDS.interest
+ *    cardPenalty             違約金            ORDS.fine
+ *    revCreditLimit          循環信用額度       ORDS.cycle_credit_limit
+ *    revgCreditPrinBalance   循環信用本金餘額    ORDS.revolving_principal
+ *    autoPayAccount          自動扣繳帳號       ORDS.autopay_account
+ *    paidAmountOnDueDate     繳款截止日扣繳金額  ORDS.autopay_amt
+ * }
  */
-export const getTransactionDetails = async (request) => {
-  // const response = await callAPI('/api/', request);
-  // const response = await new Promise((resolve) => resolve({ data: mockTransactions(request) }));
-
-  const queryCardBillRt = await callAPI('/api/card/v1/getBillSummary', request);
-
-  /* 將回傳資料轉換成頁面資料結構 */
-  const transactionDetails = queryCardBillRt.data.details.map((detail) => ({
-    txnDate: detail.txDate,
-    description: detail.desc,
-    targetAcct: detail.cardNo,
-    amount: detail.amount,
-    currency: 'NTD',
-  }));
-
-  return transactionDetails;
+export const getBillDetail = async (request) => {
+  const response = await callAPI('/api/card/v1/getBillDetail', request);
+  return response.data;
 };
 
 /**
- * 取得帳單資訊：更多帳單資訊Accoridan
-   @param {
-    "period": 期別 (ex: 202207)
-  }
-   @returns {
-     "currency": 信用卡帳單幣別 // 'NTD'
-     "amount": 本期應繳金額 // newBalance
-     "minAmount": 最低應繳金額 // minDueAmount
-     "invoiceDate": 帳單結帳日 // billClosingDate
-     "billDate": 繳費截止日 // payDueDate
-     "prevAmount": 上期應繳金額 // prevBalance
-     "prevDeductedAmount": 已繳/退金額 // paidRefundAmount
-     "newAmount": 本期新增款項 // newPurchaseAmount
-     "rate": 利息 // interestFee
-     "fine": 違約金 // cardPenalty
-     "credit": 循環信用額度 // revCreditLimit
-     "creditAvailable": 循環信用本金餘額 // revgCreditPrinBalance
-     "bindAccountNo": 自動扣款帳號 // autoPayAccount
-     "deductAmount": 繳款截止日扣款金額 // paidAmountOnDueDate
-   }
+ * 查詢會員自扣狀態，決定帳單提示內容與下方Button種類
+ * 透過 queryCardInfo 查詢本期繳款截止日
+ *
+ * @param token
+ * @return {
+ *    autoDeductStatus:       0:尚未設定自扣，1:已設定自扣
+ *    hintToPay:              繳費提醒
+ * }
+ * @throws ParseException
+ *
+ * TODO: 尚未有完整的測資，所以個電文先帶有測資的 id/帳號
  */
-export const getBillDetails = async (request) => {
-  // Assume backend store Terms as escaped HTML...
-  // const response = await new Promise((resolve) => resolve({ data: mockBillDetails }));
-
-  const billDetail = await callAPI('/api/card/v1/getBillDetail', request);
-
-  /* 將回傳資料轉換成頁面資料結構 */
-  const billDetails = {
-    currency: 'NTD',
-    amount: billDetail.data.newBalance,
-    minAmount: billDetail.data.minDueAmount,
-    invoiceDate: billDetail.data.billClosingDate,
-    billDate: billDetail.data.payDueDate,
-    prevAmount: billDetail.data.prevBalance,
-    prevDeductedAmount: billDetail.data.paidRefundAmount,
-    newAmount: billDetail.data.newPurchaseAmount,
-    rate: billDetail.data.interestFee,
-    fine: billDetail.data.cardPenalty,
-    credit: billDetail.data.revCreditLimit,
-    creditAvailable: billDetail.data.revgCreditPrinBalance,
-    bindAccountNo: billDetail.data.autoPayAccount,
-    deductAmount: billDetail.data.paidAmountOnDueDate,
-  };
-
-  return billDetails;
-  // return response.data;
+export const getBillDeducStatus = async (request) => {
+  const response = await callAPI('/api/card/v1/getBillDeducStatus', request);
+  return response.data;
 };
 
 /**
@@ -141,20 +91,9 @@ export const getInvoice = async (format) => {
     await downloadCSV('/api/deposit/v1/getDepositBook', request, `${filename}.csv`);
   }
   */
-  // alert('待串接API', format === 1 ? '下載PDF' : '下載EXCEL');
   await showCustomPrompt({
     title: '待串接API',
     message: `${format === 1 ? '下載PDF' : '下載EXCEL'}`,
     noDismiss: true,
   });
-};
-
-/**
- * 取得信用卡注意事項
- * @returns
- */
-export const getCreditCardTerms = async () => {
-  // Assume backend store Terms as escaped HTML...
-  const response = await new Promise((resolve) => resolve({ data: decodeURI(mockCreditCardTerms) }));
-  return response.data;
 };
