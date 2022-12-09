be75444bb88a0c8c9d3201cc7e6625a91718639f
Jerry Yeh <jerry88634@feib.com.tw>
2022-12-09 11:18:47 +0800

    修正：回存誤刪的 jsconfig.json

--- src/stores/reducers/index.js
+++ src/stores/reducers/index.js
@@ -1,16 +1,16 @@
 import { combineReducers } from 'redux';
-import { reducers as loginReducer } from 'proto/Login/stores';
-import { reducers as billPayReducer } from 'pages/BillPay/stores';
-import { reducers as patternLockSettingReducer } from 'pages/PatternLockSetting/stores';
-import { reducers as spinnerReducer } from 'components/Spinner/stores';
+// import { reducers as loginReducer } from 'proto/Login/stores';
+// import { reducers as billPayReducer } from 'pages/BillPay/stores';
+// import { reducers as patternLockSettingReducer } from 'pages/PatternLockSetting/stores';
+// import { reducers as spinnerReducer } from 'components/Spinner/stores';
 
 import ModalReducer from './ModalReducer';
 
 const reducer = combineReducers({
-  login: loginReducer,
-  billPay: billPayReducer,
-  patternLockSetting: patternLockSettingReducer,
-  spinner: spinnerReducer,
+  // login: loginReducer,
+  // billPay: billPayReducer,
+  // patternLockSetting: patternLockSettingReducer,
+  // spinner: spinnerReducer,
   ModalReducer,
 });
 
