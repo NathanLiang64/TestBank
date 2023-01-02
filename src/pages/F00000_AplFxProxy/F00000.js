cbdfed44fdc1dff5ec36ec9c6df7260cbf25230e
邱韋中 <qiuweizhong@qiuweizhongdeMacBook-Pro.local>
2023-01-02 22:53:13 +0800

    1. 修正 D00100_2 頁面將轉入帳號加入常用帳號時，Redux 資料被覆蓋的異常 2. 在「更多」頁面點選申請類型服務時，如未自動導向，顯示連結引導使用者點選 3. RadioGroupField 樣式調整

--- src/pages/F00000_AplFxProxy/F00000.js
+++ src/pages/F00000_AplFxProxy/F00000.js
@@ -1,27 +1,55 @@
-import React, { useEffect } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router';
 
 import Loading from 'components/Loading';
 import Layout from 'components/Layout/Layout';
 import { useNavigation } from 'hooks/useNavigation';
 import { getSSE } from './api';
+import { AplFxProxyWrapper } from './F00000.style';
 
 // NOTE 這邊作為申請平台導向用途，完成連結外開後隨即返回前一個服務
 const AplFxProxy = () => {
   const {search} = useLocation();
+  const [targetURL, setTargetURL] = useState();
+  const [openingFail, setOpeningFail] = useState(false);
   const { closeFunc } = useNavigation();
 
   useEffect(async () => {
     const prod = search.split('=')[1];
     const { sse } = await getSSE({ prod });
-    const URL = `${process.env.REACT_APP_APLFX_URL}prod=${prod}&sse=${sse}`;
-    window.open(URL, '_blank');
-    closeFunc();
+    const url = `${process.env.REACT_APP_APLFX_URL}prod=${prod}&sse=${sse}`;
+    setTargetURL(url);
+    const opener = window.open(url, '_blank');
+    if (opener) closeFunc();
+    else setOpeningFail(true);
   }, []);
+
+  const renderHintMessage = () => (
+    <p className="hint-message">
+      若未導向，請點擊此
+      <a
+        href={targetURL}
+        target="_blank"
+        onClick={() => closeFunc()}
+        rel="noreferrer noopener"
+      >
+        連結
+      </a>
+      前往申請平台
+    </p>
+  );
 
   return (
     <Layout title="更多">
-      <Loading space="both" isFullscreen isCentered />
+      <AplFxProxyWrapper>
+        {/* 若外開失敗 (沒有產生 opener)，則提示使用者點選連結 */}
+        {openingFail && targetURL ? (
+          renderHintMessage()
+        ) : (
+          <Loading space="both" isFullscreen isCentered />
+        )}
+      </AplFxProxyWrapper>
     </Layout>
   );
 };
