bbcbbe43f96e2683ba709d4e63313202e795514a
Jerry Yeh <jerry88634@feib.com.tw>
2023-01-06 00:51:57 +0800

    測試：加入 src/proto 【 不會併入 sit 分支 】

---
+++ src/proto/Login/HandShake.js
@@ -0,0 +1,48 @@
+import uuid from 'react-uuid';
+import CipherUtil from 'utilities/CipherUtil';
+import { userRequest, callAPI } from 'utilities/axios';
+import { getOsType } from 'utilities/AppScriptProxy';
+
+// 裝置開啟時去呼叫
+const handshake = async () => {
+  localStorage.clear();
+  sessionStorage.clear();
+
+  const { privateKey, publicKey } = CipherUtil.generateRSA();
+  const aesKey = CipherUtil.generateKey(256); // 使用JWT模式時，Request/Response Payload 的加解密金鑰，採用 AES256。
+  const iv = CipherUtil.generateKey(128); // IV固定為 128bits
+
+  sessionStorage.setItem('privateKey', privateKey);
+  sessionStorage.setItem('publicKey', publicKey);
+  sessionStorage.setItem('aesKey', aesKey);
+  sessionStorage.setItem('iv', iv);
+
+  const message = {
+    txnId: `WVIEW_${uuid()}`,
+    deviceId: '18157973-686F-4ACC-8EF1-4D034040A898',
+    // deviceId: uuid(),
+    // NOTE 用不用的 deviceId 可以進行「已在不同手機做過綁定」的情境。
+    osType: getOsType() - 1,
+    osVersion: '16.1.1',
+    appVersion: '1.0.26',
+    encK: aesKey,
+    iv,
+    rsaPubK: publicKey,
+  };
+  if (!message.deviceId) alert('請在 proto/Login/HandShake 填入 deviceId');
+  const getPKeyRs = await userRequest('get', '/auth/v1/getPublicKey');
+  sessionStorage.setItem('serverPKey', getPKeyRs.data.data);
+
+  //
+  // 精誠隨想的 Handshak
+  //
+  const preloadRs = await callAPI('/smJwe/v1/preload', message);
+  if (preloadRs.code === '0000') {
+    const { jwtToken } = preloadRs.data;
+    sessionStorage.setItem('jwtToken', jwtToken);
+  } else {
+    alert(`Hand shake fail! ${preloadRs.message}`);
+  }
+};
+
+export default handshake;
---
+++ src/proto/Login/login.api.js
@@ -0,0 +1,56 @@
+import uuid from 'react-uuid';
+import { callAPI } from 'utilities/axios';
+import e2ee from 'utilities/E2ee';
+
+export const login = async (data) => {
+  const payload = {
+    txnId: `WVIEW_${uuid()}`,
+    custId: data.identity.toUpperCase(),
+    username: e2ee(data.account),
+    password: e2ee(data.password),
+    rsaPubK: sessionStorage.getItem('publicKey'),
+    isRepeat: !!data.isRepeat,
+  };
+  const loginRs = await callAPI('/smJwe/v1/login', payload);
+  if (loginRs.code === '0000') {
+    const response = loginRs.data;
+    const { result, errCode, message } = response;
+
+    if (result === true) return true;
+
+    if (errCode === 'E004') {
+      if (window.confirm(message)) {
+        const relogin = {
+          ...data,
+          isRepeat: true,
+          isgToken: response.isgToken,
+        };
+        return await login(relogin);
+      }
+
+      alert('已取消登入');
+    } else alert(message);
+  } else alert(loginRs.message);
+
+  return false;
+};
+
+export const personalDataPreload = async (request) => {
+  const response = await callAPI('/auth/preload', request);
+  return response;
+};
+
+export const getInitData = async () => {
+  const response = await callAPI('/smApi/v1/getInitData');
+  return response;
+};
+
+export const getAnnouncementData = async () => {
+  const response = await callAPI('/smApi/v1/getAnnouncementData');
+  return response;
+};
+
+export const getHomeData = async () => {
+  const response = await callAPI('/smApi/v1/getHomeData');
+  return response;
+};
