bff1aa54081cfe9ffa6bb042c7dd4958fe5778a4
Jerry Yeh <jerry88634@feib.com.tw>
2023-03-03 00:27:52 +0800

    新增：C002 社群帳本API （部份）

--- src/proto/B00100/ledgerApi.js
+++ src/proto/B00100/ledgerApi.js
@@ -5,43 +5,49 @@ import { callAPI } from 'utilities/axios';
  * 社群帳本API測試
  */
 export const ledgerApiTest = async () => {
-  let uuidtoken = sessionStorage.getItem('ledger_uuidtoken');
-  if (!uuidtoken) {
-    // *** 一定要執行 ***
-    const getKeyRs = await callAPI('/ledger/getKey', { // 不能重複取值！
-      data: {
-        uuidtoken: null,
-        key: sessionStorage.getItem('publicKey'),
-        type: 2, // 裝置類型(1.Web, 2.iOS, 3.Android, 0.Unknown)
-      },
-    });
-    uuidtoken = getKeyRs.data.uuidtoken;
-    sessionStorage.setItem('ledger_uuidtoken', uuidtoken);
+  // let uuidtoken = sessionStorage.getItem('ledger_uuidtoken');
+  // if (!uuidtoken) {
+  //   // *** 一定要執行 ***
+  //   const getKeyRs = await callAPI('/ledger/getKey', { // 不能重複取值！
+  //     data: {
+  //       uuidtoken: null,
+  //       key: sessionStorage.getItem('publicKey'),
+  //       type: 2, // 裝置類型(1.Web, 2.iOS, 3.Android, 0.Unknown)
+  //     },
+  //   });
+  //   uuidtoken = getKeyRs.data.uuidtoken;
+  //   sessionStorage.setItem('ledger_uuidtoken', uuidtoken);
 
-    // *** 一定要執行 ***
-    // Bankee會員在登入後，進入帳本的登入
-    const bankeeLoginRs = await callAPI('/ledger/memberHadLogin');
+  // *** 一定要執行 ***
+  // Bankee會員在登入後，進入帳本的登入
+  const bankeeLoginRs = await callAPI('/ledger/login');
 
-    // // 登入（訪客？）
-    // const response = await callAPI('/ledger/memberLogin', {
-    //   data: {
-    //     token: uuidtoken,
-    //     deviceName: 'SM-S9060',
-    //     osVersion: 'Android|13|1.0.15',
-    //     push: '',
-    //     mudid: '',
-    //   },
-    // }).data;
-  }
+  // // 登入（訪客？）
+  // const response = await callAPI('/ledger/memberLogin', {
+  //   data: {
+  //     token: uuidtoken,
+  //     deviceName: 'SM-S9060',
+  //     osVersion: 'Android|13|1.0.15',
+  //     push: '',
+  //     mudid: '',
+  //   },
+  // }).data;
+  // }
 
   /*
   // 帳本首頁
-  const initListRs = await callAPI('/ledger/initList');
-  // 銀行代碼清單
-  const bankListRs = await callAPI('/ledger/bankList');
+  const initListRs = await callAPI('/ledger/getListItems');
+  // // 銀行代碼清單
+  // const bankListRs = await callAPI('/ledger/bankList');
   // 我的帳本清單(可取得 ledgerId)
-  const ledgerListRs = await callAPI('/ledger/ledgerList');
+  const getLedgerList = await callAPI('/ledger/getLedgerList');
   // 我的帳本清單
-  const ledgerListRs = await callAPI('/ledger/getLedger', { ledgerid: '470' });
+  const openLedger = await callAPI('/ledger/openLedger', { ledgerid: '470' });
   */
+
+  // 確認是否可再新增帳本
+  const checkAdd = await callAPI('/ledger/checkAdd');
+
+  // 取得可連結帳號清單、使用條款與加開子帳號權限
+  const accountList = await callAPI('/ledger/accountList', { filterbind: true });
 };
