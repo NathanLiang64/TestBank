aa26c9a0c62919f08b3eaf37170f906c787e6e49
leetw131 <leetw@jgallop.com>
2023-03-13 17:27:34 +0800

    調整：C002 邀請/要錢卡 優化、資料確認，新增要錢卡動圖及邀請卡圖片

--- src/pages/C00200_Ledger/InvitationCard/InvitationCard.js
+++ src/pages/C00200_Ledger/InvitationCard/InvitationCard.js
@@ -3,11 +3,14 @@ import { useEffect, useState } from 'react';
 import { toCurrency } from 'utilities/Generator';
 import { handleTypeText } from '../utils/usgeType';
 import PageWrapper from './InvitationCard.style';
+import JoinCard from '../Assets/joinCard.png';
+import { cardImage } from '../utils/cardImage';
 
+/**
+ * 要錢卡/邀請卡 卡面
+ */
 const InvitationCard = (cardData) => {
   const [model, setModel] = useState();
-  console.log('InvitationCard');
-  const paymentCardImage = <div style={{height: '16rem', width: '24rem', backgroundColor: 'grey'}}>image</div>;
 
   const renderInfoRow = (title, content) => (
     <div className="info_row">
@@ -18,22 +21,26 @@ const InvitationCard = (cardData) => {
 
   useEffect(() => {
     const {data} = cardData;
-    setModel(data);
+    setModel({
+      ...data.model,
+      isInviteCard: data.isInviteCard,
+    });
   }, []);
   return (
     model ? (
       <PageWrapper>
         <div>
-          {console.log({model})}
+          {console.log('InvitationCard', {model})}
           <div className="ledger_name_container">{model.ledgerName}</div>
           <div className="image_container">
-            {paymentCardImage}
+            {!model.isInviteCard && cardImage(model.imageId)}
+            {model.isInviteCard && <img src={JoinCard} alt="邀請卡" />}
           </div>
 
           {/* 要錢卡 */}
-          {model.cardType === 'REQUEST' && (
+          {!model.isInviteCard && (
             <div>
-              <div className="hint_container">{`${model.requester} 想跟 ${model.target} 收款`}</div>
+              <div className="hint_container">{`${model.sendMemberName} 想跟 ${model.receiveMemberName} 收款`}</div>
               <div className="info_container">
                 {renderInfoRow('金額', `NTD${toCurrency(model.amount)}`)}
                 {renderInfoRow('帳本', model.ledgerName)}
@@ -44,9 +51,9 @@ const InvitationCard = (cardData) => {
           )}
 
           {/* 邀請卡 */}
-          {model.cardType === 'INVITE' && (
+          {model.isInviteCard && (
             <div>
-              <div className="hint_container">{`${model.requester} 邀請你加入帳本`}</div>
+              <div className="hint_container">{`${model.sendMemberName} 邀請你加入帳本`}</div>
             </div>
           )}
         </div>
